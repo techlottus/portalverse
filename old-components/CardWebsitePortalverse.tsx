132b21a007f1f87bff438fad9a273d44d1e9c512
sifloz <hecn@live.com.mx>
2023-03-15 14:20:52 -0600

    chore: Recovered cardwebsite component implementation

--- old-components/CardWebsitePortalverse.tsx
+++ old-components/CardWebsitePortalverse.tsx
@@ -1,32 +1,32 @@
-import { FC } from "react"
-import cn from "classnames"
-import Image from "@/old-components/Image"
-import { CardWebsitePortalverseData } from "@/types/CardWebsitePortalverse.types"
-import RichtText from "./Richtext/Richtext"
-import LinkIcons from "./LinkLottus"
+import { FC } from "react";
+import Image from "@/old-components/Image";
+import RichtText from "./Richtext/Richtext";
+import LinkIcons from "./LinkLottus";
+import type { CardWebsitePortalverseData } from "@/types/CardWebsitePortalverse.types";
+import cn from "classnames";
 
 const CardWebsitePortalverse: FC<CardWebsitePortalverseData> = ({ data, classNames, onClick }: CardWebsitePortalverseData) => {
 
   return <>
     {
       data.type === "vertical"
-        ? <div className={cn("rounded-b-lg hover:shadow-2xl flex flex-col")}>
+        ? <div className={cn("rounded-b-lg hover:shadow-2xl flex flex-col h-full")}>
           <div>
             <Image classNames="w-full aspect-2/1 flex h-full" src={data.image} alt="image" />
           </div>
-          <div className={cn("px-3 flex flex-col", classNames, { "border-l-[1px] border-r-[1px] border-b-[1px] rounded-b-lg": data.border, "bg-white": data.background })}>
+          <div className={cn("px-3 flex flex-col h-full", classNames, { "border-l-[1px] border-r-[1px] border-b-[1px] rounded-b-lg": data.border, "bg-white": data.background })}>
             <h3 className="pt-3 mb-2 font-Nunito font-normal text-SC/Blackandgrey/B-60 text-[11px]">{data.subtitle?.toLocaleUpperCase()}</h3>
-            <h1 className="mb-2 font-Poppins font-semibold text-lg ">{data.title}</h1>
+            <h1 className="mb-2 font-Nunito font-bold text-base">{data.title}</h1>
             <RichtText data={{
               content: data.text
             }} classNames="mb-2" />
-            {
-              data?.isLink ?
-                <div className="flex justify-end h-full items-end mb-3">
-                  <LinkIcons data={data.link} onClick={onClick} />
-                </div>
-                : null
-            }
+            <div className="flex justify-end h-full items-end mb-3">
+              {
+                data.isLink === true
+                  ? <LinkIcons data={data.link} onClick={onClick} />
+                  : null
+              }
+            </div>
           </div>
         </div>
         : data.type === "horizontal"
@@ -50,6 +50,6 @@ const CardWebsitePortalverse: FC<CardWebsitePortalverseData> = ({ data, classNam
           : null
     }
   </>
-}
+};
 
-export default CardWebsitePortalverse
+export default CardWebsitePortalverse;
