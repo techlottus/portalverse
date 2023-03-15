ff5cfdfab57fb3bfb5d103bc1fea980286c3dc37
sifloz <hecn@live.com.mx>
2023-03-15 13:16:45 -0600

    feat: Added admissions page icons

--- old-components/Icon.tsx
+++ old-components/Icon.tsx
@@ -1,32 +1,36 @@
-import { FC, memo } from "react"
-import SearchIcon from "@/icons/search.svg"
-import MarkerIcon from "@/icons/marker.svg"
-import PhoneIcon from "@/icons/phone.svg"
-import EmailIcon from "@/icons/email.svg"
-import EyeIcon from "@/icons/eye.svg"
-import PersonIcon from "@/icons/person.svg"
-import HamburguerIcon from "@/icons/hamburguer.svg"
-import CloseIcon from "@/icons/close.svg"
-import TwitterIcon from "@/icons/twitter.svg"
-import Agreement from "@/icons/agreement.svg"
-import GraduateCap from "@/icons/graduatecap.svg"
-import Trophy from "@/icons/trophy.svg"
-import TriangleDown from "@/icons/triangledown.svg"
-import FacebookIcon from "@/icons/Facebook.svg"
-import InstagramIcon from "@/icons/Instagram.svg"
-import TikTokIcon from "@/icons/TikTok.svg"
-import YoutubeIcon from "@/icons/Youtube.svg"
-import SortIcon from "@/icons/sort.svg"
-import buscarTrabajo from "@/icons/buscarTrabajo.svg"
-import conseguirEmpleo from "@/icons/conseguirEmpleo.svg"
-import hacerContactos from "@/icons/hacerContactos.svg"
-import mejorarCurriculum from "@/icons/mejorarCurriculum.svg"
+import { FC, memo } from "react";
+import SearchIcon from "@/icons/search.svg";
+import MarkerIcon from "@/icons/marker.svg";
+import PhoneIcon from "@/icons/phone.svg";
+import EmailIcon from "@/icons/email.svg";
+import EyeIcon from "@/icons/eye.svg";
+import PersonIcon from "@/icons/person.svg";
+import HamburguerIcon from "@/icons/hamburguer.svg";
+import CloseIcon from "@/icons/close.svg";
+import TwitterIcon from "@/icons/twitter.svg";
+import Agreement from "@/icons/agreement.svg";
+import GraduateCap from "@/icons/graduatecap.svg";
+import Trophy from "@/icons/trophy.svg";
+import TriangleDown from "@/icons/triangledown.svg";
+import FacebookIcon from "@/icons/Facebook.svg";
+import InstagramIcon from "@/icons/Instagram.svg";
+import TikTokIcon from "@/icons/TikTok.svg";
+import YoutubeIcon from "@/icons/Youtube.svg";
+import SortIcon from "@/icons/sort.svg";
+import buscarTrabajo from "@/icons/buscarTrabajo.svg";
+import conseguirEmpleo from "@/icons/conseguirEmpleo.svg";
+import hacerContactos from "@/icons/hacerContactos.svg";
+import mejorarCurriculum from "@/icons/mejorarCurriculum.svg";
 import nuevasCompetencias from "@/icons/nuevasCompetencias.svg"
-import graduate from "@/icons/graduate.svg"
-import student from "@/icons/student.svg"
-import trophy from "@/icons/trophy.svg"
-import work from "@/icons/work.svg"
-import Whatsapp from "@/icons/Whatsapp.svg"
+import graduate from "@/icons/graduate.svg";
+import student from "@/icons/student.svg";
+import trophy from "@/icons/trophy.svg";
+import work from "@/icons/work.svg";
+import Whatsapp from "@/icons/Whatsapp.svg";
+import Aniversario55 from "@/icons/55_aniversario.svg";
+import Egresados from "@/icons/egresados.svg";
+import Internacionalizacion from "@/icons/internacionaizacion.svg";
+import Udg from "@/icons/udg.svg";
 
 const iconTypes: any = {
   search: SearchIcon,
@@ -55,7 +59,11 @@ const iconTypes: any = {
   graduateIcon: graduate,
   studentIcon: student,
   workIcon: work,
-  whatsapp: Whatsapp
+  whatsapp: Whatsapp,
+  aniversario55: Aniversario55,
+  egresados: Egresados,
+  internacionalizacion: Internacionalizacion,
+  udg: Udg,
 };
 
 const IconComponent: FC<any> = memo(({ name, ...props }: any) => {
