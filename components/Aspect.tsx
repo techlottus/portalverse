ebda4259e642715368a045ce21c96e4af242ef04
Lalo <hecn@live.com.mx>
2023-04-04 12:06:06 -0600

    chore: add comments

--- components/Aspect.tsx
+++ components/Aspect.tsx
@@ -1,16 +1,24 @@
+/**
+ * This implementation for Safari-friendly Aspect Ratio Boxes is based on:
+ * https://css-tricks.com/aspect-ratio-boxes/
+ */
+
 type AspectProps = {
   ratio: string;
   children: any;
 };
 
-const defaultRatioPercentage = "50%"; // 2/1
+// Aspect ratio is expressed as a padding-top percentage, where a ratio of 2:1 gives a padding-top percentage of 1 / 2 * 100 = 50%
+const defaultRatioPercentage = "50%"; // 2:1
+
+// Matches a fraction of either integer or floating point numbers, excluding numbers starting with 0
 const fractionRegex = /([1-9][0-9]*(.[0-9]*)?)\/[1-9][0-9]*(.[0-9]*)?/g;
 
 const calculateAspectRatioPercentage = (ratio: string) => {
   const containsValidFraction = fractionRegex.test(ratio);
 
   if (containsValidFraction) {
-    const fraction = ratio.match(fractionRegex)?.[0];
+    const fraction = ratio?.match(fractionRegex)?.[0];
     const numerator = Number(fraction?.split("/")?.[0]);
     const denominator = Number(fraction?.split("/")?.[1]);
 
@@ -33,7 +41,7 @@ const Aspect = (props: AspectProps) => {
   };
 
   return (
-    <div style={paddingStyles} className="relative h-0">
+    <div style={paddingStyles} className="relative w-full h-0">
       <div className="w-full h-full absolute top-0 left-0">{children}</div>
     </div>
   );
