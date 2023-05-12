e4ba0161308dcff3d3793a95c482fa132e0918aa
Lalo <eduardo.j.a@ciencias.unam.mx>
2023-05-12 14:48:57 -0600

    feat: adjust path validation

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -32,7 +32,7 @@ export async function getStaticProps(context: any) {
 }
 
 const isValidPath = (path: string) => {
-  return !!path && !path?.includes("//");
+  return !!path.trim() && !path?.includes("//") && !path?.includes(" ");
 };
 
 // https://stackoverflow.com/questions/19134860/javascript-remove-strings-in-beginning-and-end
