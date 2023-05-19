1b3d1be3d33d617bd72b3bb298778faaf903f57f
Lalo <hecn@live.com.mx>
2023-05-19 11:57:14 -0600

    chore: add comment

--- utils/routes.ts
+++ utils/routes.ts
@@ -2,5 +2,9 @@ export const isValidPath = (path: string) => {
   return !!path.trim() && !path?.includes("//") && !path?.includes(" ");
 };
 
-// https://stackoverflow.com/questions/19134860/javascript-remove-strings-in-beginning-and-end
+/**
+ * Normalizes the path format by removing any slashes at the beginning and end of a path. For instance,
+ * "/a/b", "a/b/" and "/a/b/" all end up as "a/b" 
+ * Taken from: https://stackoverflow.com/questions/19134860/javascript-remove-strings-in-beginning-and-end
+ */
 export const normalizePath = (path: string) => path?.replace(/(^\/+|\/+$)/gm, "");
