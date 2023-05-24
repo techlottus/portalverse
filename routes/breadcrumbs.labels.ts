18e933f1a1e72d1f6e6fd9cd12f6b162c0b2c4ec
Mabel Galindo <mabel.galindo@lottuseducation.com>
2023-05-24 17:58:45 -0600

    feat: add breadcumb labels to new pages

--- routes/breadcrumbs.labels.ts
+++ routes/breadcrumbs.labels.ts
@@ -29,6 +29,14 @@ const BreadcrumbsLabels: any = {
   "vida-estudiantil": "Vida Estudiantil",
   "alumnos": "Alumnos",
   "SIAAF": "SIAAF",
+  "programa-titulacion-segura": "Programa Titulacion Segura",
+  "servicio-social-practicas-profesionales-voluntarias": "Servicio Social Practicas Profesionales Voluntarias",
+  "servicio-social-lic-semestrales": "Servicio Social Licenciaturas Semestrales UdeG",
+  "servicio-social-lic-cuatrimestrales": "Servicio Social Licenciaturas Cuatrimestrales",
+  "bachillerato-tecnico-sej": "Servicio Social Bachillerato Técnico SEJ",
+  "modalidades-requisitos-y-reglamentos-de-titulacion": "Modalidades, requisitos y reglamentos de titulación",
+  "convocatorias-titulacion-comunicados": "Convocatorias de Titulación y Comunicados",
+  "carta-pasante": "Carta de Pasante",
   ...Routes["oferta-educativa"].reduce((prev: any, value: any) => {
     const {params: {programs}} = value
     const programList = programs.reduce((p:any, c:any) => {
