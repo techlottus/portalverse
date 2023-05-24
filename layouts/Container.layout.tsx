d2420efcfe192e96245da73b0cf0fa427174aa45
Lalo <eduardo.j.a@ciencias.unam.mx>
2023-05-24 15:35:45 -0600

    chore: update Container layout tag element

--- layouts/Container.layout.tsx
+++ layouts/Container.layout.tsx
@@ -2,7 +2,7 @@ import cn from "classnames"
 import ContentLayoutProps from "@/types/ContentLayout.types"
 
 export default function Container({ children, classNames } : ContentLayoutProps) {
-  return <section className={cn("max-w-d-base mx-auto w-d-base:px-6", classNames)}>
+  return <div className={cn("max-w-d-base mx-auto w-d-base:px-6", classNames)}>
     { children }
-  </section>
+  </div>
 }
