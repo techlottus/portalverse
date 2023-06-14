6d6f66f5cd05ee83586fed35b50123408083d404
Lalo <eduardo.j.a@ciencias.unam.mx>
2023-06-14 12:59:13 -0600

    feat: create FormVideo section renderer component

---
+++ components/sections/FormVideo.tsx
@@ -0,0 +1,63 @@
+import OpenForm from "@/forms/container/OpenForm";
+import Container from "@/layouts/Container.layout";
+import Video from "@/old-components/Video";
+import type { FormVideoSection } from "@/utils/strapi/sections/FormVideo";
+
+type OpenFormStepOneConfig = {
+  title: string;
+  subtitle: string;
+  conditions: string;
+  privacyLink: { link: string; label: "Aviso de privacidad" };
+};
+
+const FormVideo = (props: FormVideoSection) => {
+  const {
+    formTitle,
+    formDescription,
+    privacyPolicyText,
+    privacyPolicy,
+    formImage,
+    videoTitle,
+    youtubeId,
+  } = props;
+
+  return (
+    <section>
+      <Container>
+        <div className="grid grid-cols-12-gap w-t:grid-cols-8-gap w-p:grid-cols-4-gap gap-grid-gap">
+          <div className="col-span-6 w-t:col-span-8 w-p:col-span-4 w-d:mt-8">
+            <OpenForm
+              config={{
+                title: formTitle,
+                subtitle: formDescription,
+                conditions: privacyPolicyText,
+                privacyLink: {
+                  link: privacyPolicy?.data?.attributes?.url || "",
+                  label: "Aviso de Privacidad",
+                },
+              }}
+              pathThankyou={`/thank-you`}
+              image={{
+                src: formImage?.data?.attributes?.url,
+                alt: formImage?.data?.attributes?.alt || "image-person",
+              }}
+            />
+          </div>
+          <div className="col-span-6 w-t:col-span-8 w-p:col-span-4 w-d:mt-8">
+            <p className="mb-6 text-Poppins font-semibold text-[22px] w-p:text-6">
+              {videoTitle}
+            </p>
+            {
+              <Video
+                dimensions={["330px", "400px", "200px"]}
+                data={{ options: { id: youtubeId, type: "", controls: true } }}
+              />
+            }
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default FormVideo;
