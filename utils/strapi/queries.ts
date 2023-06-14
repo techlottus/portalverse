6722759f4b65a178cc19f9dc68aea7c818c41812
Lalo <eduardo.j.a@ciencias.unam.mx>
2023-06-14 11:33:20 -0600

    feat: update Renderers and Queries dictionaries

--- utils/strapi/queries.ts
+++ utils/strapi/queries.ts
@@ -7,6 +7,7 @@ import { HERO_SLIDER } from "@/utils/strapi/sections/HeroSlider";
 import { LEADERBOARD } from "@/utils/strapi/sections/Leaderboard";
 import { LINK_LIST } from "@/utils/strapi/sections/LinkList";
 import { LIST_CONFIG } from "@/utils/strapi/sections/Listconfig";
+import { OVERLAY_CARD_LIST } from "@/utils/strapi/sections/OverlayCardList";
 import { PODCAST_LIST } from "@/utils/strapi/sections/PodcastList";
 import { RICH_TEXT_IMAGE } from "@/utils/strapi/sections/RichTextImage";
 import { TEXT_CONTENT } from "@/utils/strapi/sections/TextContent";
@@ -26,16 +27,16 @@ import type { TextContentSection } from "@/utils/strapi/sections/TextContent";
 export type ComponentSection =
   | AlertSection
   | BannerSection
+  | BlogPostsPodcastSection
   | ContactTargetListSection
   | FAQSection
+  | HeroSliderSection
   | LeaderboardSection
   | LinkListSection
-  | RichTextImageSection
-  | TextContentSection
-  | BlogPostsPodcastSection
   | ListconfigSection
   | PodcastListSection
-  | HeroSliderSection
+  | RichTextImageSection
+  | TextContentSection
 
 export const SECTIONS = `
   ${ALERT}
@@ -47,11 +48,8 @@ export const SECTIONS = `
   ${LINK_LIST}
   ${LIST_CONFIG}
   ${LEADERBOARD}
+  ${OVERLAY_CARD_LIST}
   ${PODCAST_LIST}
   ${RICH_TEXT_IMAGE}
   ${TEXT_CONTENT}
-  ${BLOG_POSTS_PODCAST}
-  ${LIST_CONFIG}
-  ${PODCAST_LIST}
-  ${HERO_SLIDER}
 `;
