5a650daee728ab5dae2476641db6f0dbb53b79bc
Lalo <eduardo.j.a@ciencias.unam.mx>
2023-06-15 16:31:55 -0600

    feat: create getDynamicPagesBreadcrumbs function

--- utils/routes.ts
+++ utils/routes.ts
@@ -1,3 +1,5 @@
+import type { DynamicPageData } from "@/utils/getPagesData";
+
 export const isValidPath = (path: string) => {
   return !!path?.trim() && !path?.includes("//") && !path?.includes(" ");
 };
@@ -8,3 +10,21 @@ export const isValidPath = (path: string) => {
  * Taken from: https://stackoverflow.com/questions/19134860/javascript-remove-strings-in-beginning-and-end
  */
 export const normalizePath = (path: string) => path?.replace(/(^\/+|\/+$)/gm, "");
+
+export const getDynamicPagesBreadcrumbs = (
+  pages: Array<DynamicPageData>
+): Record<string, string> => {
+  const breadcrumbs: Record<string, string> = {};
+
+  pages?.forEach((page) => {
+    const slug = normalizePath(page?.attributes?.slug);
+    const pathSegments = slug?.split("/");
+    const lastPathSegment = pathSegments?.[pathSegments?.length - 1];
+
+    const breadcrumbLabel = page?.attributes?.breadcrumb || "";
+
+    breadcrumbs[lastPathSegment] = breadcrumbLabel;
+  });
+
+  return breadcrumbs;
+};
