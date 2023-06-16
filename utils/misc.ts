c3168f7e83952bda8cf7e2b0ecd801f4f6fac622
Lalo <eduardo.j.a@ciencias.unam.mx>
2023-06-16 15:11:15 -0600

    chore: add missing optional chaining operators

--- utils/misc.ts
+++ utils/misc.ts
@@ -3,5 +3,5 @@
  * https://stackoverflow.com/questions/70287406/how-to-replace-all-accented-characters-with-english-equivalents
  */
 export const normalizeString = (string: string) => {
-  return string.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  return string?.normalize('NFD')?.replace(/[\u0300-\u036f]/g, '');
 }
