04b53e7eaba9df50c1cfc18dc040b84e9b6a8f16
Lalo <hecn@live.com.mx>
2023-06-21 18:54:18 -0600

    feat: conditionally render program detail pages content

--- components/DynamicProgramContent.tsx
+++ components/DynamicProgramContent.tsx
@@ -1,7 +1,26 @@
-const DynamicProgramContent = () => {
-  return (
-    <div>DynamicProgramContent</div>
-  )
-}
+import { Fragment } from "react";
+import type { DynamicProgramDetailData } from "@/utils/pages";
 
-export default DynamicProgramContent
+const DynamicProgramContent = (props: DynamicProgramDetailData) => {
+
+  const programAttributes = props?.program?.attributes;
+  const level = programAttributes.level?.data?.attributes?.title;
+
+  const renderContent = () => {
+    switch(level) {
+      case "Educación Continua": {
+        return <h1>Agregar Renderer de Educación Continua</h1>
+      }
+      case "Bachillerato": {
+        return <h1>Agregar Renderer de Bachillerato</h1>
+      }
+      default: {
+        return <h1>Agregar Renderer de Nivel Superior</h1>
+      }
+    }
+  }
+
+  return <Fragment>{renderContent()}</Fragment>;
+};
+
+export default DynamicProgramContent;
