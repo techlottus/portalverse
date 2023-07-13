c23ae2dba9cd3b7ef2bf9e2bcb235102e604dc18
Lalo <eduardo.j.a@ciencias.unam.mx>
2023-07-13 17:27:28 -0600

    feat: add category filter on getBlogPosts query

--- utils/getBlogPosts.ts
+++ utils/getBlogPosts.ts
@@ -5,15 +5,26 @@ export type BlogPostsVariables = {
   start?: number;
   limit?: number;
   sort?: "publication_date:desc" | "publication_date:asc";
+  category?: string;
 };
 
-const getBlogPosts = async (variables: BlogPostsVariables) => {
-  const { start = 0, limit, sort } = variables;
+const getBlogPosts = async (variables: BlogPostsVariables = {}) => {
+
+  console.log("Inside getBlogPosts");
+  console.log("variables", variables);
+
+  const start = variables?.start || 0;
+  const limit = variables?.limit;
+  const sort = variables?.sort || "publication_date:desc";
+  const category = variables?.category;
+
+  console.log("category", category);
 
   const data = await fetchStrapiGraphQL<BlogPostsData>(BLOG_POSTS, {
     start,
     limit,
     sort,
+    category
   });
 
   return data;
@@ -37,8 +48,12 @@ export type BlogPostsData = {
 }
 
 const BLOG_POSTS = `
-query BlogPosts ($start: Int, $limit: Int, $sort: [String]) {
-  blogPosts(pagination: { start: $start, limit: $limit }, sort: $sort){
+query BlogPosts ($start: Int, $limit: Int, $sort: [String], $category: String) {
+  blogPosts(
+    pagination: { start: $start, limit: $limit }
+    sort: $sort
+    filters: { categories: { title: { eq: $category } } }
+  ) {
     data {
       attributes {
         title
