c60bfdb65550913834bf436e8dce96bb94a292e6
MabeGN01 <mabel.galindo@lottuseducation.com>
2023-07-27 21:39:43 -0600

    fix: replace token bg-primary500 to bg-primary-500

--- old-components/OptionPill.tsx
+++ old-components/OptionPill.tsx
@@ -9,7 +9,7 @@ const OptionPill: FC<OptionPillConfigComponent> = ({ data, active, onClick }: Op
   };
 
   return <section onClick={handleClick} className={cn("flex items-center justify-start py-1 px-[6px] rounded text-sm bg-surface-0 select-none w-p:p-0 w-p:w-40 w-p:h-10 w-auto", { 'border border-[#b0003c]': active, "cursor-not-allowed": data.disabled, "cursor-pointer": !data.disabled, })}>
-    <div className={cn("radio-option rounded-[50%] w-4 h-4 flex items-center justify-center bg-surface-500 mr-3 ml-4 w-p:hidden", { 'border border-[#b0003c] bg-primary500': active })}>
+    <div className={cn("radio-option rounded-[50%] w-4 h-4 flex items-center justify-center bg-surface-500 mr-3 ml-4 w-p:hidden", { 'border border-[#b0003c] bg-primary-500': active })}>
       <div className={cn("rounded-[50%] w-2 h-2 bg-surface-0")}></div>
     </div>
     <span className="my-3 mr-6 w-p:pl-3">{ data.name }</span>
