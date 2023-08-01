daa69b0b0d893de4add361a84196628eb6a0ec17
MabeGN01 <mabel.galindo@lottuseducation.com>
2023-08-01 17:29:06 -0600

    feat: replace token text-[14px] to text-3.5

--- forms/steps/step-one-continuouseducation.tsx
+++ forms/steps/step-one-continuouseducation.tsx
@@ -124,17 +124,17 @@ const StepOne: FC<any> = ({ classNames, data, image, onNext, programs }: any) =>
   <div className="flex gap-6">
     <div className="flex flex-col gap-6">
       <h1 className="font-texts font-bold text-5 leading-6">{ config.title }</h1>
-      <p className="font-texts font-normal text-[14px] leading-4">{ config.subtitle }</p>
+      <p className="font-texts font-normal text-3.5 leading-4">{ config.subtitle }</p>
     </div>
     <div className={"aspect-1/1 w-t:hidden w-p:hidden"} >
       <Image classNamesImg="rounded-full" classNames="w-[112px] h-[112px]" src={image.src} alt={image.alt} />
     </div>
   </div>
   <div className="flex align-middle items-center">
-    <p className="mt-8 mb-6 text-[14px] leading-5 text-surface-800 font-texts font-normal mr-1">
+    <p className="mt-8 mb-6 text-3.5 leading-5 text-surface-800 font-texts font-normal mr-1">
       { config.conditions }{" "}
       <Link href={config?.privacyLink?.link || ""} passHref target={"_blank"}>
-        <span className="mt-8 mb-6 text-[14px] font-texts font-normal text-sm text-surface-800 ">{config?.privacyLink?.label}</span>
+        <span className="mt-8 mb-6 text-3.5 font-texts font-normal text-sm text-surface-800 ">{config?.privacyLink?.label}</span>
       </Link>
     </p>
   </div>
@@ -153,7 +153,7 @@ const StepOne: FC<any> = ({ classNames, data, image, onNext, programs }: any) =>
     <Input errorMessage={configControls.errorMessagesStepOneOpenFormContinuous.email} hasError={errorControls.email} eventFocus={() => handleTouchedControl("email")} data={ configControls.inputEmailOpenFormStepOne } eventKeyPress={(e: CustomEvent) => handleKeyPress(e, "email")} />
   </div>
   <div className="mt-6">
-    <p className="font-texts font-normal text-[14px] leading-5">{ config.programs }</p>
+    <p className="font-texts font-normal text-3.5 leading-5">{ config.programs }</p>
     <Select onClick={(option: CustomEvent) => handleOptionSelected(option)} options={dataPrograms} data={{ ...SelectInit, textDefault: !!infoControls.program ? "  " : "Elige un programa", disabled: !dataPrograms.length }}  />
     <p className={cn("text-error-500 text-xs px-3 mt-4", { "hidden": !errorControls.program })}>{ configControls.errorMessagesStepOneOpenFormContinuous.program }</p>
   </div>
--- forms/steps/step-one-openform.tsx
+++ forms/steps/step-one-openform.tsx
@@ -115,16 +115,16 @@ const StepOne: FC<any> = ({ classNames, data, image, onNext, step, config: stepO
     <div className="flex gap-6">
       <div className="flex flex-col gap-6">
         <h1 className="font-texts font-bold text-5 leading-6">{ config.title }</h1>
-        <p className="font-texts font-normal text-[14px] leading-4">{ config.subtitle }</p>
+        <p className="font-texts font-normal text-3.5 leading-4">{ config.subtitle }</p>
       </div>
       <div className="w-p:hidden">
         <Image classNamesImg="w-full h-full object-cover" classNames="w-[112px] h-[112px] rounded-full overflow-hidden" src={image.src} alt={image.alt} />
       </div>
     </div>
     <div className="flex align-middle items-center mt-8 mb-6">
-      <p className="text-[14px] leading-5 text-surface-800 font-texts font-normal mr-1">{ config.conditions }</p>
+      <p className="text-3.5 leading-5 text-surface-800 font-texts font-normal mr-1">{ config.conditions }</p>
       <Link href={config.privacyLink.link} passHref target={"_blank"}>
-        <p className="text-[14px] font-texts font-normal text-sm text-surface-800 underline">{config.privacyLink.label}</p>
+        <p className="text-3.5 font-texts font-normal text-sm text-surface-800 underline">{config.privacyLink.label}</p>
       </Link>
     </div>
     <div className="mb-6">
@@ -146,7 +146,7 @@ const StepOne: FC<any> = ({ classNames, data, image, onNext, step, config: stepO
         <Input errorMessage={configControls.errorMessagesStepOneOpenForm.email} hasError={errorControls.email} eventFocus={() => handleTouchedControl("email")} data={ configControls.inputEmailOpenFormStepOne } eventKeyPress={(e: CustomEvent) => handleKeyPress(e, "email")} />
       </div>
       <div className="mt-6">
-        <p className="font-texts font-normal text-[14px] leading-5">{ config.modality }</p>
+        <p className="font-texts font-normal text-3.5 leading-5">{ config.modality }</p>
         <Select onClick={(option: CustomEvent) => handleOptionSelected(option)} options={[...dataModalities]} data={{ ...SelectInit, textDefault: !!infoControls.modality ? " " : "Elige una modalidad", icon: "school" }}  />
         <p className={cn("text-error-500 text-xs px-3 mt-4", { "hidden": !errorControls.modality })}>{ configControls.errorMessagesStepOneOpenForm.modality }</p>
       </div>
