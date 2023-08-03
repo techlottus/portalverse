30f0e87586d68afe3d9f65afb7588b1e098381e1
MabeGN01 <mabel.galindo@lottuseducation.com>
2023-08-02 18:29:38 -0600

    feat: add token 16 to lineHeight on fetch multitenant

--- utils/fetch-multitenant.js
+++ utils/fetch-multitenant.js
@@ -209,6 +209,7 @@ async function populateTailwind  () {
       "12": "3rem",
       "12.5": "3.125rem",
       "13": "3.75rem",
+      "16": "4rem",
       "16.25": "4.063rem",
       "semi-tight": "1.125"
     },
