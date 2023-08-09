ae8acafdc8deb43fbf6672d18a20db10e73d7c00
MabeGN01 <mabel.galindo@lottuseducation.com>
2023-08-08 23:59:06 -0600

    feat: replace token w-[80%] to w-4/5

--- old-components/CarouselCards/CarouselCards.tsx
+++ old-components/CarouselCards/CarouselCards.tsx
@@ -42,7 +42,7 @@ const CarouselCards: FC<any> = ({ data: { items }, classNames, classNameSlide }:
 
   return <section className={cn("flex h-full relative w-full", classNames)}>
     <span aria-label="prev" onClick={handlerClickControl} className={cn("z-10 left-0", { "bg-surface-500": items.length === 1, "bg-surface-0 cursor-pointer": items.length > 1 }, stylesBaseControls)}>arrow_back_ios</span>
-    <section className="overflow-x-auto flex gap-6 w-d:w-[80%] w-t:w-[95%] w-p:w-[95%] mx-auto">
+    <section className="overflow-x-auto flex gap-6 w-d:w-4/5 w-t:w-[95%] w-p:w-[95%] mx-auto">
       {
         items.map((item: any, i: number) => <section key={`card-item-${i}`} className="grow w-full h-10 bg-primary-500">
           {/* <CardWebsite key={`card-${i}`} data={item}/> */}
