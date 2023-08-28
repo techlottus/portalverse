513eeaf90a6c37ce4d02b8c36d18dbfecc288431
alan llamas <alanllamasg@gmail.com>
2023-08-28 18:00:55 -0500

    feat: added default lottus values

--- utils/fetch-multitenant.js
+++ utils/fetch-multitenant.js
@@ -2,8 +2,120 @@ const fs = require('fs')
 const dotenv = require('dotenv').config();
 const { env } = require('process')
 
+const lottusColors = {
+  primary: {
+    intensity0: '#e3fffc',
+    intensity50: '#c6fff8',
+    intensity100: '#8dfff1',
+    intensity200: '#55ffea',
+    intensity300: '#1cffe3',
+    intensity400: '#00e2c6',
+    intensity500: '#00a994',
+    intensity600: '#008d7b',
+    intensity700: '#007163',
+    intensity800: '#00554a',
+    intensity900: '#003831',
+    intensity950: '#001c19',
+  },
+  secondary: {
+    intensity0: '#ced4de',
+    intensity50: '#9caabd',
+    intensity100: '#7f8ea5',
+    intensity200: '#63738e',
+    intensity300: '#485977',
+    intensity400: '#2e4061',
+    intensity500: '#13294b',
+    intensity600: '#10223f',
+    intensity700: '#0d1b32',
+    intensity800: '#0a1426',
+    intensity900: '#060e19',
+    intensity950: '#03070d',
+  },
+  surface: {
+    intensity0: '#FFFFFF',
+    intensity50: '#FAFAFA',
+    intensity100: '#F4F4F5',
+    intensity200: '#E4E4E7',
+    intensity300: '#D4D4D8',
+    intensity400: '#A1A1AA',
+    intensity500: '#71717A',
+    intensity600: '#52525B',
+    intensity700: '#3F3F46',
+    intensity800: '#27272A',
+    intensity900: '#18181B',
+    intensity950: '#000000',
+  },
+  error:{
+    intensity0: '#EFF4FC',
+    intensity50: '#DFE9F9',
+    intensity100: '#C0D2F4',
+    intensity200: '#A0BCEE',
+    intensity300: '#80A6E8',
+    intensity400: '#618FE3',
+    intensity500: '#4179DD',
+    intensity600: '#2460CA',
+    intensity700: '#1D4DA2',
+    intensity800: '#163979',
+    intensity900: '#0E2651',
+    intensity950: '#071328',
+  },
+  success:{
+    intensity0: '#FDF9F0',
+    intensity50: '#FAF3E1',
+    intensity100: '#F6E8C3',
+    intensity200: '#F1DCA6',
+    intensity300: '#ECD088',
+    intensity400: '#ECD088',
+    intensity500: '#E3B94C',
+    intensity600: '#DAA722',
+    intensity700: '#AF861B',
+    intensity800: '#836414',
+    intensity900: '#57430D',
+    intensity950: '#2C2107',
+  },
+  warning:{
+    intensity0: '#FDF4F2',
+    intensity50: '#FBE8E5',
+    intensity100: '#F6D1CC',
+    intensity200: '#F2BAB2',
+    intensity300: '#EEA398',
+    intensity400: '#E98C7F',
+    intensity500: '#E57565',
+    intensity600: '#DD4B36',
+    intensity700: '#BC3320',
+    intensity800: '#8D2718',
+    intensity900: '#5E1A10',
+    intensity950: '#2F0D08',
+  },
+  info:{
+    intensity0: '#EEF9EC',
+    intensity50: '#DDF2D9',
+    intensity100: '#BCE6B3',
+    intensity200: '#9AD98C',
+    intensity300: '#78CC66',
+    intensity400: '#56C040',
+    intensity500: '#459A33',
+    intensity600: '#39802A',
+    intensity700: '#2E6722',
+    intensity800: '#224D19',
+    intensity900: '#173311',
+    intensity950: '#0B1A08',
+  },
+}
+
+const lottusFonts = {
+  headings: { google_font_url: 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap', font_names: [{ name: 'Poppins'}] },
+  texts: { google_font_url: 'https://fonts.googleapis.com/icon?family=Nunito+Sans:wght@400&display=swap', font_names: [{ name: 'Nunito Sans'}] },
+  icons: { google_font_url: 'https://fonts.googleapis.com/icon?family=Material+Icons', font_names: [{ name: 'Material Icons'}] }
+}
+
+const lottusLogos = {
+  logo: '../public/images/lottus.png',
+  favicon: '/public/images/lottus-favicon.png',
+}
+
 const setcolors = (colors) => {
-  return Object.keys(colors) .reduce((acc, key) => {
+  return Object.keys(colors).reduce((acc, key) => {
     acc = {
       ...acc,
       [key]: {
@@ -27,23 +139,30 @@ const setcolors = (colors) => {
 }
 
 async function fetchColors(){
-  const rawColors = await
-  fetch(`${env.NEXT_PUBLIC_MULTITENANT_URL}/pallete?populate[0]=primary,secondary,surface,complementary.colors,contextual.success,contextual.error,contextual.warning,contextual.info`, {
-    headers: {
-      "Authorization": `Bearer ${env.NEXT_PUBLIC_MULTITENANT_TOKEN}`
-    }
-  })
-
-  const Colors = await rawColors.json() 
-  const { data : { attributes: { primary, secondary, surface, contextual:{id, ...restcontextual}, complementary } } } = Colors
-
-  const complementaryColors = complementary.reduce((acc, {token, colors}) => {
-    const { id, ...rest } = colors
-    acc = {...acc, [token]: { ...rest}}
-    return acc
-  }, {})
-
-  return {primary, secondary, surface, ...restcontextual, ...complementaryColors}
+  try {
+    
+    const rawColors = await
+    fetch(`${env.NEXT_PUBLIC_MULTITENANT_URL}/pallete?populate[0]=primary,secondary,surface,complementary.colors,contextual.success,contextual.error,contextual.warning,contextual.info`, {
+      headers: {
+        "Authorization": `Bearer ${env.NEXT_PUBLIC_MULTITENANT_TOKEN}`
+      }
+    })
+  
+    const Colors = await rawColors.json() 
+    const { data : { attributes: { primary, secondary, surface, contextual:{id, ...restcontextual}, complementary } } } = Colors
+  
+    const complementaryColors = complementary.reduce((acc, {token, colors}) => {
+      const { id, ...rest } = colors
+      acc = {...acc, [token]: { ...rest}}
+      return acc
+    }, {})
+  
+    return {primary, secondary, surface, ...restcontextual, ...complementaryColors}
+  } catch (error) {
+    // console.error(error)
+    console.info('using lottus default colors')
+    return lottusColors
+  }
 }
 
 const setFonts = (fonts) => {
@@ -71,26 +190,32 @@ const setFonts = (fonts) => {
 }
 
 async function fetchFonts(){
-
-  const rawFonts = await
-  fetch(`${env.NEXT_PUBLIC_MULTITENANT_URL}/font?populate[0]=headings,texts,extra_fonts,headings.font_names,texts.font_names,extra_fonts.font,extra_fonts.font.font_names`, {
-    headers: {
-      "Authorization": `Bearer ${env.NEXT_PUBLIC_MULTITENANT_TOKEN}`
-    }
-  })
-
-  const Fonts = await rawFonts.json() 
-  const { data : { attributes: { headings, texts, extra_fonts } } } = Fonts
-
-  const extraFonts = extra_fonts.reduce((acc, {token, font}) => {
-    const { id, ...rest } = font
-    acc = {...acc, [token]: rest}
-    return acc
-  }, {})
-  const fonts = { headings: headings[0], texts: texts[0], ...extraFonts }
+  try {
+    const rawFonts = await
+    fetch(`${env.NEXT_PUBLIC_MULTITENANT_URL}/font?populate[0]=headings,texts,extra_fonts,headings.font_names,texts.font_names,extra_fonts.font,extra_fonts.font.font_names`, {
+      headers: {
+        "Authorization": `Bearer ${env.NEXT_PUBLIC_MULTITENANT_TOKEN}`
+      }
+    })
   
-  return fonts
+    const Fonts = await rawFonts.json() 
+    const { data : { attributes: { headings, texts, extra_fonts } } } = Fonts
+  
+    const extraFonts = extra_fonts.reduce((acc, {token, font}) => {
+      const { id, ...rest } = font
+      acc = {...acc, [token]: rest}
+      return acc
+    }, {})
+    const fonts = { headings: headings[0], texts: texts[0], ...extraFonts }
+    
+    return fonts
+  } catch (error) {
+    // console.error(error);
+    console.info('using lottus default fonts')
+    return lottusFonts
+  }
 }
+
 const setLogos = (logos) => {
   return Object.keys(logos) .reduce((acc, token) => {
     acc.css[token] = `url('${logos[token]}')`
@@ -101,30 +226,36 @@ const setLogos = (logos) => {
 }
 
 async function fetchLogos(){
-
-  const rawLogos = await
-  fetch(`${env.NEXT_PUBLIC_MULTITENANT_URL}/logo?populate=logo,favicon,extra_logos,extra_logos.image`, {
-    headers: {
-      "Authorization": `Bearer ${env.NEXT_PUBLIC_MULTITENANT_TOKEN}`
-    }
-  })
-
-  const Logos = await rawLogos.json() 
-  const { data : { attributes: { logo, favicon, extra_logos } } } = Logos
-
+  try {
+    const rawLogos = await
+    fetch(`${env.NEXT_PUBLIC_MULTITENANT_URL}/logo?populate=logo,favicon,extra_logos,extra_logos.image`, {
+      headers: {
+        "Authorization": `Bearer ${env.NEXT_PUBLIC_MULTITENANT_TOKEN}`
+      }
+    })
   
-  const extraLogos = extra_logos.reduce((acc, {logo_token, image}) => {    
-    const { data: { id, attributes: { url } } } = image
-    acc = {...acc, [logo_token]: url}
-    return acc
-  }, {})
-
-  return {
-    logo: logo.data.attributes.url,
-    favicon: favicon.data.attributes.url,
-    ...extraLogos
+    const Logos = await rawLogos.json() 
+    const { data : { attributes: { logo, favicon, extra_logos } } } = Logos
+  
+    
+    const extraLogos = extra_logos.reduce((acc, {logo_token, image}) => {    
+      const { data: { id, attributes: { url } } } = image
+      acc = {...acc, [logo_token]: url}
+      return acc
+    }, {})
+  
+    return {
+      logo: logo.data.attributes.url,
+      favicon: favicon.data.attributes.url,
+      ...extraLogos
+    }
+  } catch (error) {
+    // console.error(error);
+    console.info('using lottus logos');
+    return lottusLogos;
   }
 }
+
 const setConfig = ({ styles_safelist, environment_variables }) => {
   
   const safelist = styles_safelist.map(({ token }) => token)
@@ -222,6 +353,9 @@ async function populateTailwind  () {
       ...tailwindColors
     },
     fontFamily: {
+      Poppins: ['Poppins'],
+      Nunito: ['Nunito'],
+      'Nunito-Sans': ['Nunito Sans'],
       ...tailwindFonts.fonts
     },
     gridTemplateColumns: {
@@ -286,7 +420,11 @@ module.exports = {
       console.error(err);
     }
   });
-  fs.writeFile('./styles/fonts.css', tailwindFonts.links, 'utf-8', (err) => {
+  fs.writeFile('./styles/fonts.css', `${tailwindFonts.links}
+    \n@import url('https://fonts.googleapis.com/css2?&family=Poppins:wght@400;600;700&display=swap');
+    @import url('https://fonts.googleapis.com/icon?family=Nunito+Sans:wght@400&display=swap');
+    @import url('https://fonts.googleapis.com/icon?family=Nunito:wght@400&display=swap');
+    `, 'utf-8', (err) => {
     if (err) {
       console.error(err);
     }
@@ -298,6 +436,5 @@ module.exports = {
   });
 
 }
-
 
 populateTailwind()
