9f18affe4837da149e2e1e313205c6b379cdfbbe
Lalo <eduardo.j.a@ciencias.unam.mx>
2023-09-01 18:19:37 -0600

    feat: create graduates form useToken hook

---
+++ utils/forms/graduates/hooks/useToken.ts
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import type { TokenResponseData } from "@/api/graduates/getToken";
+
+type Status = {
+  data?: TokenResponseData;
+  isLoading: boolean;
+  isSuccess: boolean;
+  isError: boolean;
+};
+
+const defaultStatus: Status = {
+  isLoading: false,
+  isSuccess: false,
+  isError: false
+}
+
+const useToken = () => {
+
+  const [status, setStatus] = useState<Status>({ ...defaultStatus });
+
+  const getTokenData = async (): Promise<TokenResponseData | undefined> => {
+    setStatus({ ...defaultStatus, isLoading: true });
+
+    try {
+      const response = await fetch("/api/graduates/getToken");
+
+      if (!response.ok) throw new Error();
+
+      const data = await response?.json() as TokenResponseData;
+      const token = data?.token;
+
+      if(!token) throw new Error();
+
+      setStatus({ ...status, data, isLoading: false, isSuccess: true });
+      return data;
+    } catch {
+      setStatus({ ...status, isLoading: false, isError: true });
+      return;
+    }
+
+  };
+
+  return {
+    data: status?.data,
+    isLoading: status?.isLoading,
+    isError: status?.isError,
+    getTokenData,
+  };
+};
+
+export default useToken;
