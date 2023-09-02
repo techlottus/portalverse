79c0ea46eaf5ba6f7cea72e48aa842a68fa21f67
Lalo <eduardo.j.a@ciencias.unam.mx>
2023-09-01 18:20:22 -0600

    feat: create graduates form useEnrollmentIdValidation hook

---
+++ utils/forms/graduates/hooks/useEnrollmentIdValidation.ts
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import type {
+  EnrollmentIdValidationRequestData,
+  EnrollmentIdValidationResponseData,
+} from "@/api/graduates/validateEnrollmentId";
+
+type Status = {
+  data?: EnrollmentIdValidationResponseData;
+  isLoading: boolean;
+  isSuccess: boolean;
+  isError: boolean;
+};
+
+const defaultStatus: Status = {
+  isLoading: false,
+  isSuccess: false,
+  isError: false
+}
+
+const useEnrollmentIdValidation = () => {
+
+  const [status, setStatus] = useState<Status>({ ...defaultStatus });
+
+  const validateEnrollmentId = async (
+    token: string = "",
+    enrollmentId: string = ""
+  ): Promise<EnrollmentIdValidationResponseData | undefined> => {
+
+    console.log("inside validateEnrollmentId");
+
+    if (!isAllowedEnrollmentIdValue(enrollmentId)) {
+
+      console.log("enrollmentIdValue is not allowed");
+
+      /**
+       * "0" and empty strings ("" and "   ") are valid enrollment id values that exist in the database for testing purposes,
+       * so we need to consider these as invalid.
+       */
+      const data: EnrollmentIdValidationResponseData = {
+        isValid: false,
+      };
+      setStatus({ ...status, data, isSuccess: true });
+      return data;
+    }
+
+    console.log("enrollmentIdValue is allowed");
+
+    setStatus({ ...defaultStatus, isLoading: true });
+
+    const body: EnrollmentIdValidationRequestData = {
+      token,
+      data: {
+        enrollmentId,
+      },
+    };
+
+    try {
+      const response = await fetch("/api/graduates/validateEnrollmentId", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(body),
+      });
+
+      if (!response.ok) throw new Error();
+
+      const data = await response?.json() as EnrollmentIdValidationResponseData;
+
+      setStatus({
+        ...status,
+        data,
+        isLoading: false,
+        isSuccess: true,
+      });
+
+      return data;
+
+    } catch {
+      setStatus({ ...status, isLoading: false, isError: true });
+      return;
+    }
+  };
+
+  return {
+    data: status?.data,
+    isLoading: status?.isLoading,
+    isError: status?.isError,
+    validateEnrollmentId,
+  };
+};
+
+/**
+ * "0" and empty strings ("" and "   ") are valid enrollment id values that exist in UANE's database for testing purposes,
+ * so we need to consider these as invalid.
+ */
+const isAllowedEnrollmentIdValue = (value: string) => {
+  return !!value?.trim() && value?.trim() !== "0";
+}
+
+export default useEnrollmentIdValidation;
