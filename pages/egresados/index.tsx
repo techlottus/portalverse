11a18381521da1e6ad7dd39ea7030451427b6bea
Lalo <hecn@live.com.mx>
2023-09-01 18:23:33 -0600

    feat: add GraduatesForm component in /egresados page

--- pages/egresados/index.tsx
+++ pages/egresados/index.tsx
@@ -6,6 +6,7 @@ import ContentFullLayout from "@/layouts/ContentFull.layout"
 import NextPageWithLayout from "@/types/Layout.types"
 import Button from "@/old-components/Button/Button"
 import BannerNumeralia from "@/old-components/BannerNumeralia/BannerNumeralia"
+import GraduatesForm from "@/forms/container/GraduatesForm"
 import { getDataPageFromJSON } from "@/utils/getDataPage"
 import RichtText from "@/old-components/Richtext/Richtext"
 import Cintillo from "@/old-components/Cintillo"
@@ -29,14 +30,18 @@ const Egresados: NextPageWithLayout = ({ sections, meta }: any) => {
         <div className="head col-span-12 w-t:col-span-8 w-p:col-span-4 border-solid border-2 w-t:hidden w-p:hidden">
           <BannerNumeralia data={sections.head.banner} />
         </div>
-        <div className="col-span-6 w-t:col-span-8 w-p:col-span-4 w-p:order-2">
-          <p className="font-Poppins font-bold leading-[125%] text-10 w-t:text-6 w-p:text-6 mb-6"> {sections.introduccion.title}</p>
-          <RichtText data={{
-            content: sections.introduccion.descripcion
-          }} />
-        </div>
-        <div className="col-span-6 w-t:col-span-4 w-p:col-span-4 w-p:order-1 relative">
-          {/* <BeWantedForm pathThankyou={`/thank-you?type=egresados`} classNames="absolute w-full h-auto bg-white bottom-0 rounded-lg" /> */}
+        <div className="col-span-12 w-t:col-span-8 w-p:col-span-4 grid grid-cols-2 w-p:gap-12 gap-6">
+          <div className="w-p:col-span-2 w-p:order-2">
+            <p className="font-Poppins font-bold leading-[125%] text-10 w-t:text-6 w-p:text-6 mb-6"> { sections.introduccion.title}</p>
+            <RichtText data={{
+              content: sections.introduccion.descripcion
+            }} />
+          </div>
+          <div className="w-p:col-span-2 w-p:order-1">
+            <div className="w-p:-mt-56 -mt-40 w-t:mr-6 w-d:mr-6">
+              <GraduatesForm />
+            </div>
+          </div>
         </div>
         <div className="col-span-6 w-t:col-span-8 w-p:col-span-4">
           <Video dimensions={["330px", "400px", "200px"]} data={sections.video.video} />
