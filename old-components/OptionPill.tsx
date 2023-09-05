826a592439a95183cf1a9d75322bea3cf64a8d41
alan llamas <alanllamasg@gmail.com>
2023-09-05 14:40:56 -0500

    chore: added important to progress bar and added bg to pill

--- old-components/OptionPill.tsx
+++ old-components/OptionPill.tsx
@@ -10,7 +10,7 @@ const OptionPill: FC<OptionPillConfigComponent> = ({ data, active, onClick }: Op
   };
 
   return <section onClick={handleClick} className={cn("relative flex items-center justify-start py-1 px-[6px] rounded text-sm bg-surface-100 select-none w-p:p-0 w-p:w-40 w-p:h-10 w-auto border", { "border-transparent": !active, "border-primary-500": active && !data?.disabled, "cursor-not-allowed": data?.disabled, "cursor-pointer": !data?.disabled, })}>
-    <div className={cn("radio-option rounded-[50%] w-4 h-4 flex items-center justify-center bg-surface-500 mr-3 ml-4 w-p:hidden", { 'border border-primary-500 bg--primary-500': active && !data?.disabled })}>
+    <div className={cn("radio-option rounded-[50%] w-4 h-4 flex items-center justify-center mr-3 ml-4 w-p:hidden", { 'border border-primary-500 bg-primary-500': active && !data?.disabled, 'bg-surface-500': !active })}>
       <div className={cn("rounded-[50%] w-2 h-2 bg-surface-100")}></div>
     </div>
     <span className="my-3 mr-6 w-p:pl-3">{ data.name }</span>
