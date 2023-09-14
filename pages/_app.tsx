8bf6411625bb47daa310f4db023c1904cba4c223
alan llamas <alanllamasg@gmail.com>
2023-09-14 12:45:53 -0500

    chore: modified meta pixel script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -46,14 +46,18 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
       ></Pixel>
       <Pixel
         ID="meta-pixel"
-        script={`window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '481987629436592', {
-            page_path: window.location.pathname,
-          });`}
-          pixel="https://www.facebook.com/tr?id=481987629436592&amp;ev=PageView&amp;noscript=1"
-          image={true}
+        script={`!function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', 487461332128996);
+          fbq('track', 'PageView');`}
+        pixel="https://www.facebook.com/tr?id=481987629436592&amp;ev=PageView&amp;noscript=1"
+        image={true}
         ></Pixel>
 
 
