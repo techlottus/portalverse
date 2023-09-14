f92f21e4a6fa2f7c088e8f923df98045728045b9
alan llamas <alanllamasg@gmail.com>
2023-09-14 12:24:43 -0500

    chore: moved out noscript from head

--- components/Pixel/index.tsx
+++ components/Pixel/index.tsx
@@ -1,20 +1,17 @@
 
 import React from 'react'
-import Head  from 'next/head'
 import Script from 'next/script'
 
 export default ({script, pixel, ID}: { script?: string, pixel?: string, ID?: string }) => {
   return(
     <>
       { !!script && <Script id={ID} strategy='afterInteractive' dangerouslySetInnerHTML={{ __html: script }}/>}
-      <Head>
         { !!pixel &&
           <noscript>
             <iframe src={pixel} height="0" width="0" style={{"display":"none","visibility":"hidden"}}></iframe>
       
           </noscript>
         }
-      </Head>
     </>
   )
 }
