792511beb625d1d04828e657919588bac5521da4
alan llamas <alanllamasg@gmail.com>
2023-09-13 19:04:29 -0500

    chore: implemented tag manager and meta pixel with pixel component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import Head from 'next/head'
 import "@/styles/globals.scss"
 import { AppPropsWithLayout } from "@/types/Layout.types"
 import * as gtag from "@/lib/gtag"
+import Pixel from "@/components/Pixel"
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const router = useRouter();
@@ -33,37 +34,20 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 
   return getLayout(
     <>
-      <Script id='google-tag-manager'
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    <Pixel ID="google-tag-manager" script={`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-5ZVRHTS');`,
-      }}/>
-      <Script id='meta-pixel'
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+        })(window,document,'script','dataLayer','GTM-5ZVRHTS');`} pixel="https://www.googletagmanager.com/ns.html?id=GTM-5ZVRHTS"></Pixel>
+    <Pixel ID="meta-pixel" script={`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', '481987629436592', {
               page_path: window.location.pathname,
             });
-          `,
-      }}/>
-      {/* // // Meta Pixel Code (noscript) */}
-      {/* <noscript>
-        <img height="1" width="1" style={{"display":"none","visibility":"hidden"}} src="https://www.facebook.com/tr?id=481987629436592&amp;ev=PageView&amp;noscript=1"/>
-      </noscript> */}
-      {/* // //End Meta Pixel Code (noscript) */}
-      {/* // // Google Tag Manager (noscript) */}
-      <noscript>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5ZVRHTS" height="0" width="0" style={{"display":"none","visibility":"hidden"}}></iframe>
-      </noscript>
-      {/* // // End Google Tag Manager (noscript) */}
+          `} pixel="https://www.facebook.com/tr?id=481987629436592&amp;ev=PageView&amp;noscript=1"></Pixel>
+
 
       <Component {...pageProps} />
     </>)
