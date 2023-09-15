c6aa1f209be4d75584aabbb65fa7c9a2afdbf032
alan llamas <alanllamasg@gmail.com>
2023-09-15 18:02:21 -0500

    chore: implemented dynamic script pixels in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react"
-import Script from "next/script"
 import { useRouter } from "next/router"
-import Head from 'next/head'
 import "@/styles/globals.scss"
 import { AppPropsWithLayout } from "@/types/Layout.types"
 import * as gtag from "@/lib/gtag"
 import * as fbq from '@/lib/fb-pixel'
 import Pixel from "@/components/Pixel"
+import { getGeneralConfigData } from "@/utils/getGeneralConfigData"
+import { ScriptsPixels } from "@/utils/strapi/sections/ScriptPixel"
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const router = useRouter();
-
+  const [scripts, setScripts] = useState<any>();
   useEffect(() => {
     const handleRouteChange = (url: any) => {
       gtag.pageview(url);
@@ -25,42 +25,35 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   }, [router.events]);
 
   useEffect( () => {
+    const getConfigData = async () => {
+      const generalConfig = await getGeneralConfigData();
+      const scripts = generalConfig?.generalConfig?.data?.attributes?.scriptsPixels || [];
+      setScripts(scripts)
+    }
+   getConfigData()
+  }, [])
+  useEffect( () => {
     // we need import elements with commonJS
     if (typeof window !== 'undefined') {
       require("@lottus23/lottus-elements-uane/elements")
     }
   }, [])
-
+  
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page)
+  
 
   return getLayout(
     <>
-      <Pixel 
-        name="google-tag-manager"
-        script={`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5ZVRHTS');`}
-        pixel={{src: "https://www.googletagmanager.com/ns.html?id=GTM-5ZVRHTS", element: "iframe"}}
-      ></Pixel>
-      {/* <Pixel
-        name="meta-pixel-pageView"
-        script={`!function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', 481987629436592);
-          fbq('track', 'PageView');`
-        }
-        pixel={{src: "https://www.facebook.com/tr?id=481987629436592&ev=PageView&noscript=1", element:'img'}}
-      ></Pixel> */}
-
+      {
+        scripts && scripts.map(({name, script, pixel}: ScriptsPixels, i: number) => <Pixel
+        key={i}
+        name={name}
+        script={script}
+        pixel={{src: pixel?.src, element: pixel.element}}
+      ></Pixel>)
+      }
+    
 
       <Component {...pageProps} />
     </>)
