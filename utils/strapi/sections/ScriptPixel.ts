dbb2b96ebc2d63499230d251955905bcae285b85
alan llamas <alanllamasg@gmail.com>
2023-09-19 20:59:13 -0500

    chore: made types optional

--- utils/strapi/sections/ScriptPixel.ts
+++ utils/strapi/sections/ScriptPixel.ts
@@ -2,18 +2,18 @@
 
 export type PixelComponent = {
   name: string;
-  script: string;
-  pixel: Pixel | null
-  enabled: boolean;
-  triggerOnRouteChange: "gtagPageview" | "fbqPageview" | null
+  script?: string;
+  pixel?: Pixel | null
+  enabled?: boolean;
+  triggerOnRouteChange?: "gtagPageview" | "fbqPageview" | null
 }
 export type ScriptsPixels  = {
   type: 'ComponentSectionsScriptPixel'
   name: string;
-  script: string;
-  pixel: Pixel | null
-  enabled: boolean;
-  triggerOnRouteChange: "gtagPageview" | "fbqPageview" | null
+  script?: string;
+  pixel?: Pixel | null
+  enabled?: boolean;
+  triggerOnRouteChange?: "gtagPageview" | "fbqPageview" | null
 };
 export type Pixel = {
   src?: string;
