74617d047e97fec0f3bd24243918340c89d24626
Héctor Sifuentes <hecn@live.com.mx>
2023-10-06 19:06:51 -0600

    chore: Added ProgramAccordionListSection to queries list

--- utils/strapi/queries.ts
+++ utils/strapi/queries.ts
@@ -6,6 +6,7 @@ import { CARD_LIST } from "@/utils/strapi/sections/CardList";
 import { CONTACT_TARGET_LIST } from "@/utils/strapi/sections/ContactTargetList";
 import { CONT_ED_PROGRAMS } from "@/utils/strapi/sections/ContEdPrograms";
 import { FAQ_SECTION } from "@/utils/strapi/sections/FAQ";
+import { FORM_CONTAINER } from "@/utils/strapi/sections/ContainerForm";
 import { FORM_VIDEO } from "@/utils/strapi/sections/FormVideo";
 import { GOOGLE_MAP } from "@/utils/strapi/sections/GoogleMap";
 import { HERO_SLIDER } from "@/utils/strapi/sections/HeroSlider";
@@ -14,11 +15,12 @@ import { LEADERBOARD } from "@/utils/strapi/sections/Leaderboard";
 import { LINK_LIST } from "@/utils/strapi/sections/LinkList";
 import { LIST_CONFIG } from "@/utils/strapi/sections/Listconfig";
 import { MODALITY_FILTER } from "@/utils/strapi/sections/ModalityFilter";
+import { OUTSTANDING_LIST } from "@/utils/strapi/sections/OutstandingList";
 import { OVERLAY_CARD_LIST } from "@/utils/strapi/sections/OverlayCardList";
 import { PODCAST_LIST } from "@/utils/strapi/sections/PodcastList";
+import { PROGRAM_ACCORDION_LIST,  } from "@/utils/strapi/sections/ProgramAccordionItems";
 import { PROGRAMS_FILTER } from "@/utils/strapi/sections/ProgramsFilter";
 import { PROMO_LINK_LIST } from "@/utils/strapi/sections/PromoLinkList";
-import { OUTSTANDING_LIST } from "@/utils/strapi/sections/OutstandingList";
 import { RICH_TEXT_IMAGE } from "@/utils/strapi/sections/RichTextImage";
 import { RICH_TEXT_VIDEO } from "@/utils/strapi/sections/RichTextVideo";
 import { STATISTICS_CARD_LIST } from "@/utils/strapi/sections/StatisticsCardList";
@@ -29,6 +31,7 @@ import type { BannerSection } from "@/utils/strapi/sections/Banner";
 import type { BlogPostsPodcastSection } from "@/utils/strapi/sections/BlogPostsPodcast";
 import type { CardListSection } from "@/utils/strapi/sections/CardList";
 import type { ContactTargetListSection } from "@/utils/strapi/sections/ContactTargetList";
+import type { ContainerForm } from "@/utils/strapi/sections/ContainerForm";
 import type { ContEdProgramsSection } from "@/utils/strapi/sections/ContEdPrograms";
 import type { FAQSection } from "@/utils/strapi/sections/FAQ";
 import type { FormVideoSection } from "@/utils/strapi/sections/FormVideo";
@@ -39,6 +42,7 @@ import type { LeaderboardSection } from "@/utils/strapi/sections/Leaderboard";
 import type { LinkListSection } from "@/utils/strapi/sections/LinkList";
 import type { ListconfigSection } from "@/utils/strapi/sections/Listconfig";
 import type { ModalityFilterSection } from "@/utils/strapi/sections/ModalityFilter";
+import type { ProgramAccordionListSection } from "@/utils/strapi/sections/ProgramAccordionItems";
 import type { OutstandingListSection } from "@/utils/strapi/sections/OutstandingList";
 import type { PodcastListSection } from "@/utils/strapi/sections/PodcastList";
 import type { ProgramsFilterSection } from "@/utils/strapi/sections/ProgramsFilter";
@@ -47,7 +51,6 @@ import type { RichTextImageSection } from "@/utils/strapi/sections/RichTextImage
 import type { RichTextVideoSection } from "@/utils/strapi/sections/RichTextVideo";
 import type { StatisticsCardListSection } from "@/utils/strapi/sections/StatisticsCardList";
 import type { TextContentSection } from "@/utils/strapi/sections/TextContent";
-import { ContainerForm, FORM_CONTAINER } from "./sections/ContainerForm";
 
 
 export type ComponentSection =
@@ -57,6 +60,7 @@ export type ComponentSection =
   | BlogPostsPodcastSection
   | CardListSection
   | ContactTargetListSection
+  | ContainerForm
   | ContEdProgramsSection
   | FAQSection
   | FormVideoSection
@@ -67,15 +71,15 @@ export type ComponentSection =
   | LinkListSection
   | ListconfigSection
   | ModalityFilterSection
+  | OutstandingListSection
   | PodcastListSection
+  | ProgramAccordionListSection
   | ProgramsFilterSection
   | PromoLinkListSection
-  | OutstandingListSection
   | RichTextImageSection
   | RichTextVideoSection
   | StatisticsCardListSection
   | TextContentSection
-  | ContainerForm
 
 export const SECTIONS = `
   ${ACCORDION_SECTION}
@@ -86,6 +90,7 @@ export const SECTIONS = `
   ${CONTACT_TARGET_LIST}
   ${CONT_ED_PROGRAMS}
   ${FAQ_SECTION}
+  ${FORM_CONTAINER}
   ${FORM_VIDEO}
   ${GOOGLE_MAP}
   ${HERO_SLIDER}
@@ -97,11 +102,11 @@ export const SECTIONS = `
   ${OUTSTANDING_LIST}
   ${OVERLAY_CARD_LIST}
   ${PODCAST_LIST}
-  ${PROMO_LINK_LIST}
+  ${PROGRAM_ACCORDION_LIST}
   ${PROGRAMS_FILTER}
+  ${PROMO_LINK_LIST}
   ${RICH_TEXT_IMAGE}
   ${RICH_TEXT_VIDEO}
   ${STATISTICS_CARD_LIST}
   ${TEXT_CONTENT}
-  ${FORM_CONTAINER}
 `;
