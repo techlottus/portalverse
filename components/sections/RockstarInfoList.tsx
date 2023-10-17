492359f4ed09a9a6b335919a3361ded4268d7c3e
Héctor Sifloz <41649003+sifloz@users.noreply.github.com>
2023-10-17 10:37:47 -0600

    chore: Added pagination to rockstar info page

--- components/sections/RockstarInfoList.tsx
+++ components/sections/RockstarInfoList.tsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import type { FC } from "react";
 import Container from "@/layouts/Container.layout"
 import RichtText from "@/old-components/Richtext/Richtext";
 import RockstarInfo from "@/components/sections/RockstarInfo";
 import parseEditorRawData from "@/utils/parseEditorRawData";
-import { RockstarInfoListSection } from "@/utils/strapi/sections/RockstarInfloList";
+import PaginatorPortalverse from "@/old-components/PaginatorPortalverse";
+import type { RockstarInfoListSection } from "@/utils/strapi/sections/RockstarInfloList";
 
 const RockstarInfoList: FC<RockstarInfoListSection> = (props: RockstarInfoListSection) => {
   const { title, description, rockstars } = props;
+  const pageSize = 16;
 
-  rockstars?.sort((a, b) => // sort programs alphabetically
+  const rockstarsData = rockstars?.sort((a, b) => // sort rockstars alphabetically
     a?.name < b?.name ? -1 : a?.name > b?.name ? 1 : 0
-  )
+  );
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const onPageChange = (page: any) => {
+    setCurrentPage(page);
+  };
+
+  if (rockstarsData?.length <= 0) {
+    return null;
+  }
+
+  const paginate = <T,>(items: Array<T>, pageNumber: number, pageSize: number) => {
+    const startIndex = (pageNumber - 1) * pageSize;
+    return items?.slice(startIndex, startIndex + pageSize);
+  };
+
+  const paginatedItems = paginate(
+    rockstarsData,
+    currentPage,
+    pageSize
+  );
 
   return (
     <section className="bg-surface-200 pb-14">
@@ -30,14 +53,17 @@ const RockstarInfoList: FC<RockstarInfoListSection> = (props: RockstarInfoListSe
         }
         {
           rockstars?.length > 0 ?
-            <div>
+            <div className="flex flex-col gap-4">
               <section className="col-span-12 grid w-d:grid-cols-4 gap-6 w-t:grid-cols-2 w-p:grid-cols-2">
                 {
-                  rockstars?.map((item: any, i: number) => <section key={`section-blog-${i}`}>
+                  paginatedItems?.map((item: any, i: number) => <section key={`section-blog-${i}`}>
                     <RockstarInfo {...item} type={"ComponentSectionsRockstarInfo"} />
                   </section>)
                 }
               </section>
+              <div className="col-span-12 w-t:col-span-8 w-p:col-span-4 my-6 flex justify-center w-p:hidden">
+                <PaginatorPortalverse items={rockstars?.length} currentPage={currentPage} pageSize={pageSize} onPageChange={onPageChange} iconNext={"chevron_right"} iconPrevious={"chevron_left"} />
+              </div>
             </div>
             : null
         }
