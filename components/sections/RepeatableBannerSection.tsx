7c22a784ec2716685790c4b209f42f8da378242b
MabeGN01 <mabel.galindo@lottuseducation.com>
2023-10-27 18:05:44 -0600

    feat: clean code, replace sections to divs

--- components/sections/RepeatableBannerSection.tsx
+++ components/sections/RepeatableBannerSection.tsx
@@ -1,9 +1,9 @@
 import { FC } from "react";
 import Container from "@/layouts/Container.layout";
 import parseEditorRawData from "@/utils/parseEditorRawData";
-import { RepeatableBanner } from "@/utils/strapi/sections/RepeatableBanner";
 import BannerPortalverseWrapper from "@/components/BannerPortalverseWrapper";
 import RichtText from "@/old-components/Richtext/Richtext";
+import type { RepeatableBanner } from "@/utils/strapi/sections/RepeatableBanner";
 
 const RepeatableBannerSection: FC<RepeatableBanner> = (props: RepeatableBanner) => {
   const { title, description, banners } = props
@@ -11,7 +11,7 @@ const RepeatableBannerSection: FC<RepeatableBanner> = (props: RepeatableBanner) 
   return (
     <section>
       <Container>
-        <section>
+        <div>
           {
             title ?
               <div className="mb-6"><p className="font-headings text-10 leading-12 w-p:text-6 w-p:leading-7 ">{title}</p></div>
@@ -28,16 +28,16 @@ const RepeatableBannerSection: FC<RepeatableBanner> = (props: RepeatableBanner) 
           }
           {
             banners?.length > 0 ?
-              <section className="grid w-d:grid-cols-2 gap-6 w-t:grid-cols-1 w-p:grid-cols-1">
+              <div className="grid w-d:grid-cols-2 gap-6 w-t:grid-cols-1 w-p:grid-cols-1">
                 {
-                  banners?.map((item: any, i: number) => <section key={`section-banners-${i}`}>
+                  banners?.map((item: any, i: number) => <div key={`section-banners-${i}`}>
                     <BannerPortalverseWrapper data={item} />
-                  </section>)
+                  </div>)
                 }
-              </section>
+              </div>
               : null
           }
-        </section>
+        </div>
       </Container>
     </section>
   )
