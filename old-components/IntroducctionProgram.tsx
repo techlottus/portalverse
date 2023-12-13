c65784d06bbcdb0b51613c11a9e9b18636c57b59
Héctor Sifloz <41649003+sifloz@users.noreply.github.com>
2023-12-12 22:43:34 -0600

    chore: Updated IntroductionProgram component

--- old-components/IntroducctionProgram.tsx
+++ old-components/IntroducctionProgram.tsx
@@ -1,57 +1,235 @@
-import { FC } from "react"
-import RichtText from "./Richtext/Richtext"
-import Container from "@/layouts/Container.layout"
-import Button from "./Button/Button"
-import Image from "./Image"
-import Aspect from "@/components/Aspect"
-import { IntroductionProgramData } from "@/types/IntroductionProgramData"
+import { FC } from "react";
+import { useRouter } from "next/router";
+import Aspect from "@/components/Aspect";
+import Button from "@/old-components/Button/Button";
+import Container from "@/layouts/Container.layout";
+import Image from "@/old-components/Image";
+import RichtText from "@/old-components/Richtext/Richtext";
 import cn from "classnames";
-import { useRouter } from "next/router"
+import type { IntroductionProgramData } from "@/types/IntroductionProgramData";
 
 const IntroductionProgram: FC<IntroductionProgramData> = (props: IntroductionProgramData) => {
 
   const router = useRouter();
 
-  const { title, label, discountPercentajeText, certificationMessage, description, offerPrice, price, periodicity, programPerks, brands, image, discount, checkoutUrl, checkoutUrlText } = props
+  const {
+    title,
+    label,
+    discountPercentajeText = "Descuento",
+    certificationMessage,
+    description,
+    offerPrice,
+    price,
+    periodicity,
+    programPerks,
+    brands,
+    image,
+    discount,
+    checkoutUrl,
+    checkoutUrlText = "Inscribirme ahora"
+  } = props
 
-  return <section>
-    <Container classNames="w-p:!p-0 w-t:!p-0 w-d-base:px-0">
-      <div className="flex w-p:flex-col w-t:flex-col gap-18">
-        <div className="w-d:w-2/3 w-full my-auto">
-          {
-            label ?
-              <div className="pb-2">
-                <p className="font-texts text-5 leading-5 text-surface-500">{label}</p>
-              </div> : null
-          }
-          {
-            title ?
-              <div className="pb-6">
-                <h3 className="text-10 font-bold font-headings leading-12 w-t:semi-tight w-p:leading-tight w-t:text-8.5 w-p:text-7.5">{title}</h3>
+  return (
+    <section className="my-0 mb-4 md:my-6">
+      <Container classNames="w-p:!p-0 w-t:!p-0 w-d-base:!px-0">
+        <div className="flex w-p:flex-col w-t:flex-col gap-18">
+          <div className="flex flex-col w-d:w-2/3 w-full my-auto gap-6">
+            <div className="flex flex-col gap-6 md:gap-8">
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-col gap-1">
+                  {
+                    label ?
+                      <p className="font-texts font-normal text-4 leading-4 text-surface-500">{label}</p>
+                      : null
+                  }
+                  {
+                    title ?
+                      <h3 className="text-10 font-bold font-headings leading-12 w-t:semi-tight w-p:leading-tight w-t:text-8.5 w-p:text-7.5">{title}</h3>
+                      : null
+                  }
+                </div>
+                {
+                  discount ?
+                    <div className="font-texts font-bold text-white text-xs py-2 px-4 w-max rounded-md bg-secondary-500 select-none">
+                      <span>{discount}% {discountPercentajeText || " Descuento"}</span>
+                    </div>
+                    : null
+                }
+                {
+                  certificationMessage ?
+                    <p className="text-secondary-500 font-texts font-bold text-base leading-5">{certificationMessage}</p>
+                    : null
+                }
+                {
+                  image ?
+                    <div className="w-d:w-1/3 w-full flex items-center w-d:hidden">
+                      <Aspect ratio={"2/1"}>
+                        <Image
+                          alt={"title"}
+                          src={image?.src}
+                          classNamesImg="w-full h-full object-cover rounded-lg"
+                          classNames="w-full h-full"
+                        />
+                      </Aspect>
+                    </div>
+                    : null
+                }
+                {
+                  description ?
+                    <div className="-mb-5">
+                      <RichtText data={{
+                        content: description
+                      }} />
+                    </div>
+                    : null
+                }
+                {
+                  programPerks?.length > 0 ?
+                    <div className="flex flex-col md:flex-row gap-4 md:gap-2.5 lg:gap-2.5 md:items-center w-full">
+                      {
+                        programPerks?.map((item, i) => (<div key={i}>
+                          <div className="flex items-center gap-2 p-2">
+                            <span className="material-icons my-auto text-surface-500 !text-[32px] select-none">{item?.attributes?.iconName}</span>
+                            <div className="flex flex-col">
+                              <p className="font-texts font-normal text-xs leading-4">{item?.attributes?.title}</p>
+                              <p className="font-texts font-bold text-base leading-5">{item?.attributes?.subtitle}</p>
+                            </div>
+                          </div>
+                        </div>))
+                      }
+                    </div>
+                    : null
+                }
               </div>
-              : null
-          }
-          {
-            discount && offerPrice ?
-              <div className={cn("text-white bg-secondary-500 py-2 px-4 w-max rounded-md mb-6",
-
-              )}>
-                <span>{discount}</span>
-                <span>{discountPercentajeText}</span>
+              <div className="flex flex-row gap-6">
+                {
+                  price || offerPrice || brands ?
+                    <div className="flex w-p:flex-col w-t:flex-col gap-6 mb:gap-0 w-full">
+                      {
+                        price || offerPrice ?
+                          <div className={cn({ "flex flex-col gap-0.5 mr-12": offerPrice !== 0 && price !== 0 })}>
+                            {
+                              offerPrice && offerPrice !== 0 ?
+                                <div className="flex">
+                                  <div className="flex items-center gap-0.5">
+                                    <span className="font-headings text-7 leading-9 font-bold">${offerPrice} MXN</span>
+                                    {
+                                      periodicity ?
+                                        <span className="font-texts font-normal text-lg leading-6 text-surface-900 my-auto">/{periodicity}</span>
+                                        : null
+                                    }
+                                  </div>
+                                </div>
+                                : null
+                            }
+                            {
+                              price && offerPrice ?
+                                <div className="flex">
+                                  <span className="font-texts font-normal text-sm line-through text-secondary-500">${price} MXN</span>
+                                </div>
+                                : price !== 0 ?
+                                  <div className="flex gap-1">
+                                    <span className="text-headings text-6 leading-9 font-bold">${price}</span>
+                                    <span className="text-headings text-6 leading-9 font-bold">MXN</span>
+                                    {
+                                      periodicity ?
+                                        <div className="flex gap-1 my-auto ml-1">
+                                          <span className="text-base leading-6 text-surface-500">/</span>
+                                          <span className="text-base leading-6 text-surface-500">{periodicity}</span>
+                                        </div>
+                                        : null
+                                    }
+                                  </div>
+                                  : null
+                            }
+                          </div>
+                          : null
+                      }
+                      {
+                        brands && brands?.length > 0 ?
+                          <div
+                            className={cn(
+                                "flex items-center gap-6",
+                                {
+                                  ["mb-5"]: !!offerPrice
+                                }
+                              )
+                            }
+                          >
+                            {
+                              brands?.length > 0 ?
+                                <>
+                                  {
+                                    brands?.[0]?.attributes?.contact ?
+                                      <div className="flex items-center gap-1">
+                                        <span className="material-icons !text-lg text-primary-500 select-none">email</span>
+                                        <a
+                                          className="font-texts font-bold text-base text-primary-500 leading-5 underline"
+                                          target="_blank"
+                                          rel="noreferrer"
+                                          href={brands?.[0]?.attributes?.contact}
+                                        >
+                                          Solicitar información
+                                        </a>
+                                      </div>
+                                      : null
+                                  }
+                                </>
+                                : null
+                            }
+                          </div>
+                          : null
+                      }
+                    </div>
+                    : null
+                }
               </div>
-              : null
-          }
-          {
-            certificationMessage ?
-              <div className="mb-6">
-                <p className="text-secondary-500 font-bold leading-5">{certificationMessage}</p>
-              </div>
-              : null
-          }
+            </div>
+            {
+              checkoutUrl ?
+                <div className="w-p:hidden w-t:hidden">
+                  <Button dark data={{
+                    id: "",
+                    type: "primary",
+                    title: checkoutUrlText || "Inscribirme ahora",
+                    size: "small",
+                    icon: "",
+                    lyIcon: false,
+                    disabled: false,
+                    isExpand: false,
+                    tagOnClick: "",
+                    test: ""
+                  }} onClick={() => {
+                    router.push(checkoutUrl)
+                  }} />
+                </div>
+                : null
+            }
+            {
+              checkoutUrl ?
+                <div className="w-d:hidden">
+                  <Button dark data={{
+                    id: "",
+                    type: "primary",
+                    title: checkoutUrlText || "Inscribirme ahora",
+                    size: "small",
+                    icon: "",
+                    lyIcon: false,
+                    disabled: false,
+                    isExpand: true,
+                    tagOnClick: "",
+                    test: ""
+                  }} onClick={() => {
+                    router.push(checkoutUrl)
+                  }} />
+                </div>
+                : null
+            }
+          </div>
           {
             image ?
-              <div className="w-d:w-1/3 w-full flex items-center w-d:hidden my-6">
-                <Aspect ratio={"2/1"}>
+              <div className="w-d:w-1/3 w-full flex items-center w-p:hidden w-t:hidden">
+                <Aspect ratio={"5/4"}>
                   <Image
                     alt={"title"}
                     src={image?.src}
@@ -62,165 +240,10 @@ const IntroductionProgram: FC<IntroductionProgramData> = (props: IntroductionPro
               </div>
               : null
           }
-          {
-            description ?
-              <div>
-                <RichtText data={{
-                  content: description
-                }} />
-              </div>
-              : null
-          }
-          {
-            programPerks?.length > 0 ?
-              <section className="grid w-d:grid-cols-3 gap-6 w-t:grid-cols-1 w-p:grid-cols-1 pb-6">
-                {
-                  programPerks?.map((item, i) => <div>
-                    <div className="flex">
-                      <span className="material-icons my-auto text-surface-500 !text-10">{item?.attributes?.iconName}</span>
-                      <div>
-                        <p className="text-xs leading-4 font-texts">{item?.attributes?.title}</p>
-                        <p className="text-base leading-5 font-texts font-bold">{item?.attributes?.subtitle}</p>
-                      </div>
-                    </div>
-                  </div>)
-                }
-              </section>
-              : null
-          }
-          {
-            price || offerPrice || brands ?
-              <div className={cn("flex w-p:flex-col w-t:flex-col pb-6")}>
-                {
-                  price || offerPrice ?
-                    <div className={cn({ "mr-12": offerPrice !== 0 && price !== 0 })}>
-                      {
-                        offerPrice && offerPrice !== 0 ?
-                          <div className="flex">
-                            <div className="flex gap-1">
-                              <span className="text-headings text-6 leading-9 font-bold">${offerPrice}</span>
-                              <span className="text-headings text-6 leading-9 font-bold">MXN</span>
-                              {
-                                periodicity ?
-                                  <div className="my-auto ml-1 flex gap-2">
-                                    <span>/</span>
-                                    <span className="text-base leading-6 text-surface-500">{periodicity}</span>
-                                  </div>
-                                  : null
-
-                              }
-                            </div>
-                          </div>
-                          : null
-                      }
-                      {
-                        price && offerPrice ?
-                          <div className="flex text-secondary-500 gap-1">
-                            <span className="line-through">${price}</span>
-                            <span className="line-through">MXN</span>
-                          </div>
-                          : price !== 0 ?
-                            <div className="flex gap-1">
-                              <span className="text-headings text-6 leading-9 font-bold">${price}</span>
-                              <span className="text-headings text-6 leading-9 font-bold">MXN</span>
-                              {
-                                periodicity ?
-                                  <div className="flex gap-1 my-auto ml-1">
-                                    <span className="text-base leading-6 text-surface-500">/</span>
-                                    <span className="text-base leading-6 text-surface-500">{periodicity}</span>
-                                  </div>
-                                  : null
-                              }
-                            </div>
-                            : null
-                      }
-                    </div>
-                    : null
-                }
-                {
-                  brands ?
-                    <section className="grid w-d:grid-cols-2 gap-6 w-t:grid-cols-2 w-p:grid-cols-2 pb-6">
-                      {
-                        brands?.length > 0 ?
-                          brands?.map((item) =>
-                            <div className="my-auto mr-12">
-                              {
-                                item?.attributes?.contact ?
-                                  <div className="flex">
-                                    <span className="material-icons text-primary-500 mr-2">email</span>
-                                    <a className="text-base font-texts text-primary-500" target="_blank" href={item?.attributes?.contact}>Solicitar información</a>
-                                  </div>
-                                  : null
-                              }
-                            </div>
-                          )
-                          : null
-                      }
-                    </section>
-                    : null
-                }
-              </div>
-              : null
-          }
-          {
-            checkoutUrl && checkoutUrlText ?
-              <div className="w-p:hidden w-t:hidden">
-                <Button dark data={{
-                  id: "",
-                  type: "primary",
-                  title: checkoutUrlText,
-                  size: "large",
-                  icon: "",
-                  lyIcon: false,
-                  disabled: false,
-                  isExpand: false,
-                  tagOnClick: "",
-                  test: ""
-                }} onClick={() => {
-                  router.push(checkoutUrl)
-                }} />
-              </div>
-              : null
-          }
-          {
-            checkoutUrl && checkoutUrlText ?
-              <div className="w-d:hidden">
-                <Button dark data={{
-                  id: "",
-                  type: "primary",
-                  title: checkoutUrlText,
-                  size: "large",
-                  icon: "",
-                  lyIcon: false,
-                  disabled: false,
-                  isExpand: true,
-                  tagOnClick: "",
-                  test: ""
-                }} onClick={() => {
-                  router.push(checkoutUrl)
-                }} />
-              </div>
-              : null
-          }
         </div>
-        {
-          image ?
-            <div className="w-d:w-1/3 w-full flex items-center w-p:hidden w-t:hidden">
-              <Aspect ratio={"5/4"}>
-                <Image
-                  alt={"title"}
-                  src={image?.src}
-                  classNamesImg="w-full h-full object-cover rounded-lg"
-                  classNames="w-full h-full"
-                />
-              </Aspect>
-            </div>
-            : null
-        }
-      </div>
-    </Container>
+      </Container>
+    </section>
+  );
+};
 
-  </section>
-}
-
-export default IntroductionProgram
+export default IntroductionProgram;
