3c054af937581862032ed7750f85269999294af9
MabeGN01 <mabel.galindo@lottuseducation.com>
2024-01-12 18:30:09 -0600

    feat: add section to RichTextImageBgImage

---
+++ utils/strapi/sections/RichTextImageBgImage.ts
@@ -0,0 +1,48 @@
+import { StrapiImage } from "@/types/strapi/common";
+import { RichTextImageSection } from "./RichTextImage";
+
+export type RichTextImageBgImageSection = {
+  type: "ComponentSectionsRichTextImageBgImage"
+  desktopBgImage?: StrapiImage;
+  tabletBgImage?: StrapiImage;
+  mobileBgImage?: StrapiImage;
+  RichTextImageComponent: RichTextImageSection;
+}
+
+export const RICHTEXTIMAGE_BGIMAGE = `
+... on ComponentSectionsRichTextImageBgImage {
+  desktopBgImage {
+    data {
+      attributes {
+        url
+      }
+    }
+  }
+  tabletBgImage {
+    data {
+      attributes {
+        url
+      }
+    }
+  }
+  mobileBgImage {
+    data {
+      attributes {
+        url
+      }
+    }
+  }
+  RichTextImageComponent:RichTextImage {
+    title
+    text
+    contentVariant
+    image {
+      data {
+        attributes {
+          url
+        }
+      }
+    }
+    imagePosition
+  }
+}`;
