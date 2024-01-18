86106d025accbb4b5dac0dff1354e5be4ce42bcb
MabeGN01 <mabel.galindo@lottuseducation.com>
2024-01-18 15:53:54 -0600

    feat: update query to get ctaText and ctaUrl and update type of PromoLinkListSection

--- utils/strapi/sections/PromoLinkList.ts
+++ utils/strapi/sections/PromoLinkList.ts
@@ -8,12 +8,16 @@ export type PromoLinkListSection = {
   type: "ComponentSectionsPromoLinkList";
   title: string;
   promoLinks: Array<PromoLink>;
+  linkText?: string;
+  linkUrl?: string;
 };
 
 export const PROMO_LINK_LIST = `
 ...on ComponentSectionsPromoLinkList {
   id,
   title,
+  linkText:ctaText,
+  linkUrl:ctaUrl,
   promoLinks(pagination: {start: 0, limit: -1}) {
     id,
     text,
