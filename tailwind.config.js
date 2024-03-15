8eb0834bf4dd3f1137f1e8f23ff29c875c7e75f2
MabeGN01 <mabel.galindo@lottuseducation.com>
2024-03-15 12:54:34 -0600

    feat: add colors white and transparent

--- tailwind.config.js
+++ tailwind.config.js
@@ -96,6 +96,27 @@ module.exports = {
       "16.25": "4.063rem",
       "semi-tight": "1.125"
    },
+   "colors": {
+      "white": "#fff",
+      "transparent": "transparent"
+   },
+   "fontFamily": {
+      "Poppins": [
+         "Poppins"
+      ],
+      "Nunito": [
+         "Nunito"
+      ],
+      "Nunito-Sans": [
+         "Nunito Sans"
+      ],
+      "headings": [
+         "Poppins"
+      ],
+      "texts": [
+         "Nunito Sans"
+      ]
+   },
    "gridTemplateColumns": {
       "4": "repeat(4, 1fr)",
       "8": "repeat(8, 1fr)",
@@ -137,6 +158,10 @@ module.exports = {
    },
    "minWidth": {
       "130": "7.5rem"
+   },
+   "backgroundImage": {
+      "logo": "url('https://bedu-staging-assets.s3.us-west-2.amazonaws.com/UTEG/logotipo_38c0857c20.svg')",
+      "favicon": "url('https://bedu-staging-assets.s3.us-west-2.amazonaws.com/UTEG/favicon_e6d152887e.ico')"
    }
 }
   },
