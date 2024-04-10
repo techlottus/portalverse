3d611dc118e3df43a80f3ba81c70407b5645f508
alan llamas <alanllamasg@gmail.com>
2024-04-10 16:25:00 -0500

    chore:a dded mopbile classes and options

--- components/sections/TextForm.tsx
+++ components/sections/TextForm.tsx
@@ -17,6 +17,53 @@ const TextForm: React.FC<TextFormSection> = (props: TextFormSection) => {
     mob,
     tab
   } = props;
+  const options = [
+    {
+      value: 'Arquitectura + Diseño Gráfico',
+      text: 'Arquitectura + Diseño Gráfico',
+      active: false
+    },
+    {
+      value: 'Contaduría + Administración',
+      text: 'Contaduría + Administración',
+      active: false
+    },
+    {
+      value: 'Contaduría + Mercadotecnia',
+      text: 'Contaduría + Mercadotecnia',
+      active: false
+    },
+    {
+      value: 'Mercadotecnia + Diseño Gráfico',
+      text: 'Mercadotecnia + Diseño Gráfico',
+      active: false
+    },
+    {
+      value: 'Administración + Mercadotecnia',
+      text: 'Administración + Mercadotecnia',
+      active: false
+    },
+    {
+      value: 'Derecho + Pedagogía',
+      text: 'Derecho + Pedagogía',
+      active: false
+    },
+    {
+      value: 'Comercio Internacional + Mercadotecnia',
+      text: 'Comercio Internacional + Mercadotecnia',
+      active: false
+    },
+    {
+      value: 'Comercio Internacional + Contaduría',
+      text: 'Comercio Internacional + Contaduría',
+      active: false
+    },
+    {
+      value: 'Administración + Comercio Internacional',
+      text: 'Administración + Comercio Internacional',
+      active: false
+    }
+  ]
   
  
   const richTextMarkup = parseEditorRawData(description);
@@ -54,19 +101,18 @@ const TextForm: React.FC<TextFormSection> = (props: TextFormSection) => {
         <div
           style={styles}
          className={classNames(
-          "flex items-center justify-around w-full  p-16 bg-origin-border md:bg-center bg-no-repeat bg-cover bg-secondary-0",
-      
+          "flex items-center justify-around w-full p-16 mobile:flex-col mobile:py-12 mobile:px-6  bg-origin-border md:bg-center bg-no-repeat bg-cover bg-secondary-0",
           "mobile:bg-[image:var(--image-mobile-url)]",
           "tablet:bg-[image:var(--image-tablet-url)]",
           "desktop:bg-[image:var(--image-desk-url)]"
           )}>
-          <div className="w-6/12 px-11">
+          <div className="w-6/12 desktop:px-11 mobile:w-full">
             <h3 className='font-headings text-5xl mb-2 font-bold' dangerouslySetInnerHTML={{ __html: accentedTitle }}></h3>
             
   
             <RichtText font="light" data={{content: richTextMarkup}} classNames='text-xl'/>
           </div>
-          <div className="w-6/12">
+          <div className="w-6/12 mobile:w-full ">
             <ContainerForm {...outterForm}></ContainerForm>
           </div>
 
