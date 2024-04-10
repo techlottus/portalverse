dc64ec94bd8992ccf1975429c7ef3e7707d3a4d9
alan llamas <alanllamasg@gmail.com>
2024-04-10 16:27:36 -0500

    chore: cleaned  text form section

--- components/sections/TextForm.tsx
+++ components/sections/TextForm.tsx
@@ -3,7 +3,6 @@ import Container from "../../layouts/Container.layout";
 import { TextFormSection } from "../../utils/strapi/sections/TextForm";
 import ContainerForm from './ContainerForm';
 import RichtText from '@/old-components/Richtext/Richtext';
-import Editor from '@/old-components/Editor/Editor';
 import parseEditorRawData from '@/utils/parseEditorRawData';
 import classNames from 'classnames';
 
@@ -64,8 +63,7 @@ const TextForm: React.FC<TextFormSection> = (props: TextFormSection) => {
       active: false
     }
   ]
-  
- 
+
   const richTextMarkup = parseEditorRawData(description);
   // console.log('title_accents: ', title_accents);
   const accentedTitle = title_accents.length < 1 
@@ -80,15 +78,7 @@ const TextForm: React.FC<TextFormSection> = (props: TextFormSection) => {
         acc = `${ splitTitle[0] }<span class="text-primary-500">${accent} </span>${ splitTitle[1] }`
         return acc
       }, '')
-      // console.log('accentedTitle: ', accentedTitle);
-      // title_accents?.reduce((acc, accent: any) => {
-      //   const base = acc.toString() === '<></>' ? title : acc.toString()
-      //   console.log('base: ', base);
-      //   console.log('accent: ', accent);
-      //   const splitTitle = base.split(accent)
-      //   console.log('splitTitle: ', splitTitle);
-      //   return <h3> <p>{ splitTitle[0] }</p><span className="text-primary-500">{accent}</span><p>{ splitTitle[1] }</p></h3>
-      // }, <></>)
+
       const styles: any = {
             "--image-desk-url": `url(${desk?.data?.attributes?.url})`,
             "--image-tablet-url": `url(${tab?.data?.attributes?.url})`,
@@ -97,15 +87,15 @@ const TextForm: React.FC<TextFormSection> = (props: TextFormSection) => {
 
   return <section>
       <Container>
-
         <div
           style={styles}
-         className={classNames(
-          "flex items-center justify-around w-full p-16 mobile:flex-col mobile:py-12 mobile:px-6  bg-origin-border md:bg-center bg-no-repeat bg-cover bg-secondary-0",
-          "mobile:bg-[image:var(--image-mobile-url)]",
-          "tablet:bg-[image:var(--image-tablet-url)]",
-          "desktop:bg-[image:var(--image-desk-url)]"
-          )}>
+          className={classNames(
+            "flex items-center justify-around w-full p-16 mobile:flex-col mobile:py-12 mobile:px-6  bg-origin-border md:bg-center bg-no-repeat bg-cover bg-secondary-0",
+            "mobile:bg-[image:var(--image-mobile-url)]",
+            "tablet:bg-[image:var(--image-tablet-url)]",
+            "desktop:bg-[image:var(--image-desk-url)]"
+          )}
+        >
           <div className="w-6/12 desktop:px-11 mobile:w-full">
             <h3 className='font-headings text-5xl mb-2 font-bold' dangerouslySetInnerHTML={{ __html: accentedTitle }}></h3>
             
@@ -117,12 +107,6 @@ const TextForm: React.FC<TextFormSection> = (props: TextFormSection) => {
           </div>
 
         </div>
-        {/* <div className="grid grid-cols-12-gap w-t:grid-cols-8-gap w-p:grid-cols-4-gap gap-grid-gap">
-          <div className="col-span-6 w-t:col-span-8 w-p:col-span-4">
-            
-            <ContainerForm {...form}></ContainerForm>
-          </div>
-        </div> */}
       </Container>
     </section>
 };
