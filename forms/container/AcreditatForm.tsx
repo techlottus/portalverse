51a6536ae12aad03313fb69b3f2fc6b8dcdd5d2a
David Vazquez Sanchez <vsjd.99@gmail.com>
2024-04-30 12:45:31 -0600

    create acreditat form and acreditat data

---
+++ forms/container/AcreditatForm.tsx
@@ -0,0 +1,464 @@
+import { useEffect, useState } from "react"
+import PersonalData from "@/forms/steps/PersonalData";
+import configControls from "@/forms/fixtures/controls"
+import axios from "axios";
+import { getTokenForms } from "@/utils/getTokenForms"
+import { getEducativeOffer } from "@/utils/getEducativeOffer"
+import ScheduleVisitData from "@/forms/steps/ScheduleVisitData";
+import { setRegisterBot } from "@/utils/saveDataForms"
+import { useRouter } from "next/router";
+import { env } from "process";
+
+const businessUnit = process.env.NEXT_PUBLIC_BUSINESS_UNIT!;
+
+// available modalities for prop modality = "Presencial" | "Online" | "Flex" | "Semipresencial"
+const getLeadModality = (modality: string) => {
+  switch (modality) {
+    case "Presencial": return "Presencial";
+    case "Online": return "Online";
+    case "Flex": return "Online"; // Applies to "UANE" and "UTEG" offer.
+    case "Semipresencial": return "Semipresencial"; // Applies to "ULA" offer.
+    default: return "";
+  }
+};
+
+type AcreditatForm = {
+  setStatus: (status: { loading: boolean, error: string, valid: boolean, success: boolean }) => void
+  submit: boolean;
+  prefilledData: {
+    name?: string;
+    last_name?: string;
+    phone?: string;
+    email?: string;
+    modality?: string;
+    campus: string;
+  };
+  options: {
+    modalities: {
+      value: string,
+      text: string,
+      active: boolean
+    }[];
+    campuses: {
+      value: string,
+      text: string,
+      active: boolean
+    }[];
+  }
+  controls?: any;
+}
+
+const AcreditatForm = (props: AcreditatForm) => {
+
+  const router = useRouter();
+  const queryParams = router?.query;
+  const { setStatus, submit, prefilledData } = props
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState('');
+  const [isValid, setIsValid] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const [selectedProgram, setselectedProgram] = useState<any>();
+  const [tokenActive, setTokenActive] = useState<string>("");
+  const [filteredPrograms, setFilteredPrograms] = useState<any>([]);
+  const [SFlevels, setSFlevels] = useState<any>([]);
+  const [SFprograms, setSFprograms] = useState<any>([]);
+  const [options, setOptions] = useState<any>(null);
+
+  const [personalData, setPersonalData] = useState({
+    name: "",
+    last_name: "",
+    phone: "",
+    email: "",
+  });
+
+  const [personalDataTouched, setPersonalDataTouched] = useState<{ [key: string]: boolean }>({
+    name: false,
+    last_name: false,
+    phone: false,
+    email: false,
+  })
+
+  const [personalDataErrors, setPersonalDataErrors] = useState({
+    name: false,
+    last_name: false,
+    phone: false,
+    email: false,
+  })
+
+  const [academicData, setAcademicData] = useState({
+    modality: "",
+    level: "",
+    program: "",
+    campus: "",
+  });
+
+  const [academicDataTouched, setAcademicDataTouched] = useState({
+    modality: false,
+    level: false,
+    program: false,
+    campus: false
+  });
+
+  const [academicDataErrors, setAcademicDataErrors] = useState({
+    modality: false,
+    level: false,
+    program: false,
+    campus: false
+  })
+
+  useEffect(() => {
+    if (submit) handleSubmit()
+  }, [submit]);
+
+  const {
+    fetchData: fetchEducativeOffer,
+    filterPrograms
+  } = getEducativeOffer();
+
+  const {
+    isLoading: isLoadingToken,
+    isError: isErrorToken,
+    token,
+  } = getTokenForms();
+
+  const filterByField = (data: any, filter: any, fields?: string[]) => {
+    return data?.reduce((acc: any[], curr: any) => {
+      if (!fields) {
+        if (!acc.includes(curr[filter])) {
+          acc = [...acc, curr[filter]]
+        }
+      } else {
+        const fieldsResult = fields.reduce((fieldacc: any, field: any) => {
+          if (!Object.keys(fieldacc).includes(curr[filter])) {
+            fieldacc[field] = curr[field]
+          }
+          return fieldacc
+        }, {});
+        acc = [...acc, fieldsResult]
+      }
+      return acc
+
+    }, [])
+  }
+
+  const getBusinessLineToFetchFrom = (businessLine: string) => {
+
+    switch (businessLine) {
+      case "UANE": {
+        return "UANE,ULA"
+      }
+      case "UTEG": {
+        return "UTEG,ULA"
+      }
+      case "ULA": {
+        return "ULA"
+      }
+      case "UTC": {
+        return "UTC,ULA"
+      }
+      default: return ""
+    }
+  }
+
+  useEffect(() => {
+    if (!!tokenActive) {
+      handleFetchEducativeOffer()
+    }
+  }, [tokenActive])
+
+  useEffect(() => {
+    if (filterPrograms) {
+      const offerByCampus = filterPrograms?.filter((program: any) => {
+        return program.idCampus === prefilledData?.campus
+      })
+      if (!offerByCampus || offerByCampus?.length === 0) {
+        setIsError('404')
+      } else {
+        setFilteredPrograms(offerByCampus)
+      }
+    }
+  }, [filterPrograms])
+
+  useEffect(() => {
+    if (filteredPrograms) {
+      const programs = filteredPrograms.map((program: any) => {
+        return businessUnit !== 'ULA' && program.lineaNegocio === 'ULA' && program.modalidad === 'Online'
+          ? { ...program, modalidad: 'Flex' }
+          : program
+      })
+
+      const periods = programs?.reduce((acc: any, program: any, index: number, arr: any[]) => {
+        if (!acc.includes(program.nombrePeriodo)) {
+          acc = [...acc, program.nombrePeriodo]
+        }
+        return acc
+      }, [])
+      const currentPeriod = periods?.sort((a: any, b: any) => Number(a.nombrePeriodo) - Number(b.nombrePeriodo))[periods.length - 1]
+      const periodPrograms = programs/* ?.filter((program: any) => {
+        return program.nombrePeriodo === currentPeriod
+      }) */
+
+      const levels = filterByField(periodPrograms, 'nivel')
+
+      setAcademicData({
+        ...academicData,
+        level: levels?.length === 1 ? levels[0] : academicData.level,
+        campus: prefilledData?.campus,
+        modality: "Presencial"
+      })
+
+      setAcademicDataTouched({
+        ...academicDataTouched,
+        level: levels?.length === 1,
+        campus: !!prefilledData?.campus,
+        modality: true
+      })
+
+      setSFlevels(levels?.map((level: any) => ({
+        value: level,
+        text: level,
+        active: levels?.length === 1 || level.idCampus === academicData.level
+      })))
+
+    }
+  }, [filteredPrograms])
+
+  useEffect(() => {
+    if (SFlevels?.length > 0) {
+      setOptions({
+        ...options,
+        levels: SFlevels
+      })
+      setAcademicData({
+        ...academicData,
+        level: SFlevels?.length === 1 ? SFlevels[0].value : academicData.level
+      })
+      setAcademicDataTouched({
+        ...academicDataTouched,
+        level: SFlevels?.length === 1 || academicDataTouched.level
+      })
+    }
+  }, [SFlevels])
+
+  useEffect(() => {
+    if (SFprograms?.length > 0) {
+      setOptions({
+        ...options,
+        programs: SFprograms
+      })
+      setAcademicData({
+        ...academicData,
+        program: SFprograms?.length === 1 ? SFprograms[0].value : academicData.program
+      })
+      setAcademicDataTouched({
+        ...academicDataTouched,
+        program: SFprograms?.length === 1 || academicDataTouched.program
+      })
+    }
+
+  }, [SFprograms])
+
+  useEffect(() => {
+    if (options && (options?.levels) && (options?.levels[0])) {
+      setIsLoading(false)
+    }
+  }, [options])
+
+  useEffect(() => {
+    if (!!academicData.program) {
+
+      const selectedProgramData = filteredPrograms?.filter((program: any) => {
+        return program.idOfertaPrograma === academicData.program
+      })
+      setselectedProgram(selectedProgramData[0])
+    }
+
+  }, [academicData.program]);
+
+  useEffect(() => {
+    setPersonalData({
+      ...personalData,
+      'name': prefilledData?.name || "",
+      'last_name': prefilledData?.last_name || "",
+      'phone': prefilledData?.phone || "",
+      'email': prefilledData?.email || "",
+    })
+    setAcademicData({
+      ...academicData,
+      campus: prefilledData?.campus || ""
+    })
+
+  }, [prefilledData])
+
+  useEffect(() => {
+    Validate()
+  }, [personalData, academicData]);
+
+  useEffect(() => {
+
+    if (!!academicData.level) {
+      const programsByLevel = filteredPrograms?.filter((program: any) => {
+        return program.nivel === academicData.level
+      })
+      const periods = programsByLevel?.reduce((acc: any, program: any, index: number, arr: any[]) => {
+        if (!acc.includes(program.nombrePeriodo)) {
+          acc = [...acc, program.nombrePeriodo]
+        }
+        return acc
+      }, [])
+      const sortedPeriods = periods?.sort((a: any, b: any) => {
+        return Number(a) - Number(b)
+      })
+      const currentPeriod = sortedPeriods[sortedPeriods.length - 1]
+      const periodPrograms = programsByLevel?.filter((program: any) => {
+        return program.nombrePeriodo === currentPeriod
+      })
+
+      const programs = filterByField(periodPrograms, 'nombrePrograma', ['nombrePrograma', 'idOfertaPrograma'])
+      const orderPrograms = programs.sort((a: any, b: any) => {
+        if (a.nombrePrograma < b.nombrePrograma) {
+          return -1;
+        }
+        if (a.nombrePrograma > b.nombrePrograma) {
+          return 1;
+        }
+        return 0;
+      })
+      setSFprograms(orderPrograms?.map((program: any) => ({
+        value: program?.idOfertaPrograma,
+        text: program?.nombrePrograma,
+        active: orderPrograms?.length === 1 || program.idOfertaPrograma === academicData.program
+      })))
+    }
+
+  }, [academicData.level]);
+
+  useEffect(() => {
+    if (!isLoadingToken && !isErrorToken && !!Object.keys(token).length) {
+      setTokenActive(`${token.token_type} ${token.access_token}`);
+    }
+  }, [isLoadingToken, isErrorToken, token]);
+
+  useEffect(() => {
+    setStatus({ loading: isLoading, error: isError, valid: isValid, success: isSuccess })
+  }, [isLoading, isError, isValid, isSuccess]);
+
+  const validatePersonalDataControl = (control: string, value: string) => {
+    if (control === 'email') {
+      return !!value.match(configControls.patternEmail)
+    }
+    if (control === 'phone') {
+      return value.trim().length === 10
+    }
+    return !!value.trim()
+  };
+
+  const validatePersonalDataControls = () => !Object.entries(personalData).map(([key, value]: any) => {
+    const validity = validatePersonalDataControl(key, value)
+    return validity
+  }).includes(false)
+
+  const validateAcademicDataControl = (control: string, value: string) => {
+    return !!value?.trim();
+  };
+
+  const validateAcademicDataControls = () => !Object.entries(academicData).map(([key, value]: any) => {
+    const validity = validateAcademicDataControl(key, value);
+    return validity
+  }).includes(false)
+
+  const handleFetchEducativeOffer = () => {
+    setIsLoading(true)
+    setFilteredPrograms([]);
+    const businessLineToFetchFrom = getBusinessLineToFetchFrom(businessUnit)
+
+    fetchEducativeOffer(process.env.NEXT_PUBLIC_EDUCATIVE_OFFER!, '', businessLineToFetchFrom, tokenActive);
+  }
+
+  const sendLeadData = async () => {
+    const endpoint = process.env.NEXT_PUBLIC_CAPTACION_PROSPECTO;
+
+    const nombre = personalData?.name;
+    const apellidoPaterno = personalData?.last_name;
+    const telefono = personalData?.phone;
+    const email = personalData?.email;
+    const lineaNegocio = selectedProgram?.lineaNegocio || env.NEXT_PUBLIC_BUSINESS_UNIT;
+    const modalidad = getLeadModality(academicData?.modality);
+    const nivel = academicData?.level;
+    const campus = academicData?.campus;
+    const programa = academicData?.program;
+    const validaRegistroBoot = setRegisterBot();
+    const source = `portal${businessUnit}`;
+    const canal = process.env.NEXT_PUBLIC_CANAL;
+    const medio = queryParams?.utm_medium;
+    const campana = queryParams?.utm_campaign;
+    const params = `nombre=${nombre}&apellidoPaterno=${apellidoPaterno}&telefono=${telefono}&email=${email}&lineaNegocio=${lineaNegocio}&modalidad=${modalidad}&nivel=${nivel}&campus=${campus}&programa=${programa}&avisoPrivacidad=true&leadSource=Digital&validaRegistroBoot=${validaRegistroBoot}&source=${source}&canal=${canal}${medio ? `&medio=${medio}` : ""}${campana ? `&campana=${campana}` : ""}`;
+
+    setIsLoading(true);
+
+    await axios.post(`${endpoint}?${params}`, {}, {
+      headers: {
+        Authorization: tokenActive,
+        'Content-Type': 'application/json;charset=UTF-8'
+      }
+    })
+      .then((res: any) => {
+        if (res?.data?.Exitoso !== "TRUE") {
+          throw new Error();
+        }
+        router.push(`/thank-you`);
+      })
+      .catch((err: any) => {
+        setIsLoading(false);
+        setIsError(err.message);
+      })
+  }
+
+  const Validate = () => {
+    const newPersonalDataErrors = {
+      name: !validatePersonalDataControl("name", personalData.name) && personalDataTouched.name,
+      last_name: !validatePersonalDataControl("last_name", personalData.last_name) && personalDataTouched.last_name,
+      phone: !validatePersonalDataControl("phone", personalData.phone) && personalDataTouched.phone,
+      email: !validatePersonalDataControl("email", personalData.email) && personalDataTouched.email,
+    }
+
+    setPersonalDataErrors({ ...newPersonalDataErrors });
+
+    const newAcademicDataErrors = {
+      program: !validateAcademicDataControl('program', academicData.program) && academicDataTouched.program,
+      level: !validateAcademicDataControl('level', academicData.level) && academicDataTouched.level,
+      campus: !validateAcademicDataControl('campus', academicData.campus) && academicDataTouched.campus,
+      modality: !validateAcademicDataControl('modality', academicData.modality) && academicDataTouched.modality
+    }
+
+    setAcademicDataErrors({ ...newAcademicDataErrors });
+    const isValidPersonalData = validatePersonalDataControls();
+    const isValidAcademicData = validateAcademicDataControls();
+    setIsValid(isValidPersonalData && isValidAcademicData)
+  }
+
+  const handleSubmit = async () => {
+    setIsLoading(true)
+    Validate()
+    if (isValid && !isError) {
+      sendLeadData()
+    }
+  }
+
+  return <form>
+    <PersonalData
+      personalData={personalData}
+      setPersonalData={setPersonalData}
+      infoControlsTouched={personalDataTouched}
+      setInfoControlsTouched={setPersonalDataTouched}
+      errorControls={personalDataErrors}
+      setErrorControls={setPersonalDataErrors}
+      validateControl={validatePersonalDataControl}
+    ></PersonalData>
+    
+  </form>
+};
+
+export default AcreditatForm;
---
+++ forms/steps/AcreditatData.tsx
@@ -0,0 +1,110 @@
+import { FC, useEffect, useState } from "react"
+import OpenFormInit from "@/forms/fixtures/openform"
+import configControls from "@/forms/fixtures/controls"
+import Select from "@/old-components/Select/Select"
+import { SelectInit } from "@/old-components/fixture"
+import cn from "classnames"
+import Input from "@/old-components/Input/Input"
+
+const ScheduleVisitData: FC<any> = ({
+  data,
+  config: stepOneConfig,
+  infoControlsTouched,
+  setInfoControlsTouched,
+  errorControls,
+  setErrorControls,
+  validateControl,
+  scheduleVisitData,
+  setScheduleVisitData,
+  options
+}: any) => {
+
+  const [config, setConfig] = useState<any>(stepOneConfig ? { ...stepOneConfig } : { ...OpenFormInit.stepone });
+
+  const [Options, setOptions] = useState<{ programs: [], modalities: [], levels: [] }>({ programs: [], modalities: [], levels: [] });
+
+  useEffect(() => {
+    setConfig({ ...config, ...data });
+  }, [data]);
+  useEffect(() => {
+    setOptions(options);
+  }, [options]);
+
+  const handleKeyPress = (e: CustomEvent, control: string) => {
+    const { detail: { value } } = e;
+    setInfoControlsTouched({ ...infoControlsTouched, [control]: true });
+    setScheduleVisitData({ ...scheduleVisitData, [control]: value });
+    setErrorControls({ ...errorControls, [control]: !validateControl(control, value, infoControlsTouched[control]) });
+  };
+
+  const handleSelect = (e: CustomEvent, control: string) => {
+    const { detail } = e;
+    if (control === 'program') {
+      const option = Options?.programs?.map((option: any) => {
+        option.active = option.value === detail
+        return option
+      })
+    }
+    if (control === 'modality') {
+      const option = Options?.modalities?.map((option: any) => {
+        option.active = option.value === detail
+        return option
+      })
+    }
+    if (control === 'level') {
+      const option = Options?.levels?.map((option: any) => {
+        option.active = option.value === detail
+
+        return option
+      })
+    }
+
+    setInfoControlsTouched({ ...infoControlsTouched, [control]: true });
+    setScheduleVisitData({ ...scheduleVisitData, [control]: detail });
+    setErrorControls({ ...errorControls, [control]: !validateControl(control, detail, infoControlsTouched[control]) });
+  };
+
+  const handleTouchedControl = (control: string) => {
+    setInfoControlsTouched({ ...infoControlsTouched, [control]: true });
+    setErrorControls({ ...errorControls, [control]: !validateControl(control, scheduleVisitData[control], true) && infoControlsTouched[control] });
+  }
+
+  return <>
+    <div className="grow mt-2 hidden">
+      <Input
+        eventFocus={() => handleTouchedControl("modality")}
+        data={configControls.inputProgram}
+        eventKeyPress={(e: CustomEvent) => handleKeyPress(e, "modality")}
+        value={scheduleVisitData?.modality}
+      />
+    </div>
+    <div className={cn("flex flex-col", { '': Options?.levels?.length > 0 && Options?.levels?.length < 2 })}>
+      <p className="font-texts font-normal text-sm leading-5 text-surface-800 mt-2 capitalize">Nivel</p>
+      <Select
+        onClick={(option: CustomEvent) => handleSelect(option, "level")}
+        options={Options?.levels || []}
+        data={{ ...SelectInit, textDefault: ` `, icon: " " }}
+      />
+      <p className={cn("text-error-400 text-xs px-3 mt-4", { "hidden": !errorControls.level })}>{configControls.errorMessagesStepTwoOpenForm.level}</p>
+    </div>
+    <div className="grow mt-2 hidden">
+      <Input
+        eventFocus={() => handleTouchedControl("campus")}
+        data={configControls.inputProgram}
+        eventKeyPress={(e: CustomEvent) => handleKeyPress(e, "campus")}
+        value={scheduleVisitData?.campus}
+      />
+    </div>
+    <div className="flex flex-col">
+      <p className="font-texts font-normal text-sm leading-5 text-surface-800 mt-2 capitalize">Programa</p>
+      <Select
+        onClick={(option: CustomEvent) => handleSelect(option, "program")}
+        options={Options?.programs || []}
+        data={{ ...SelectInit, textDefault: ` `, icon: " " }}
+      />
+      <p className={cn("text-error-400 text-xs px-3 mt-4", { "hidden": !errorControls.campus })}>{configControls.errorMessagesStepTwoOpenForm.campus}</p>
+    </div>
+  </>
+}
+
+export default ScheduleVisitData
