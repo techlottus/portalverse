a278a750e74f92a16af8c538811278149fade0b9
David Vazquez Sanchez <vsjd.99@gmail.com>
2024-05-13 19:10:50 -0600

    card payment component build

---
+++ components/sections/PaymentCard/index.tsx
@@ -0,0 +1,125 @@
+import cn from "classnames";
+import { StrapiImage } from "@/types/strapi/common";
+import { useRouter } from "next/router";
+import Button from "@/old-components/Button/Button";
+
+export type PaymentCardData = {
+  title?: string;
+  subtitle?: string;
+  perks?: Array<any>;
+  discount?: number;
+  checkout_url?: string;
+  price?: number;
+  discounted_price?: number;
+  total_payment?: number;
+  periodicity?: string;
+  featured_price?: boolean;
+  payment_provider_image?: StrapiImage;
+}
+
+const PaymentCard = (props: PaymentCardData) => {
+
+  const {
+    title,
+    subtitle,
+    perks = [],
+    discount,
+    checkout_url,
+    price,
+    discounted_price,
+    total_payment,
+    periodicity,
+    featured_price,
+    payment_provider_image } = props;
+
+  const router = useRouter();
+
+
+  return (
+    <div className={cn("gap-y-4 grid rounded-lg border border-surface-200 p-4 bg-white w-64 h-64 mobile:mx-auto", { "opacity-40": !checkout_url })}>
+      <div>
+        {
+          payment_provider_image
+            ? <img className="w-11 mb-1 rounded-md" src={payment_provider_image?.data?.attributes?.url} alt="" />
+            : null
+        }
+        {
+          payment_provider_image
+            ? <p className="font-headings text-lg font-bold">{title}</p>
+            : null
+        }
+        {
+          subtitle
+            ? <p className="font-texts text-xs font-semibold">{subtitle}</p>
+            : null
+        }
+      </div>
+
+      {
+        perks?.length > 0
+          ? <div className="flex flex-col justify-center">
+            <div className="grid gap-y-1">
+              {
+                perks?.map((perk: any, i: any) => {
+                  return (
+                    <div className="flex items-center">
+                      <span className="material-symbols-outlined !text-xs text-success-400 me-2">check_circle</span><p className="font-texts text-xs font-semibold">{perk?.accent}</p>
+                    </div>
+                  )
+                }
+                )
+              }
+            </div>
+          </div>
+          : null
+      }
+
+      <div className=" flex justify-center flex-col">
+        {
+          discount
+            ? <div className="bg-secondary-500 rounded-2xl px-3 py-1 max-w-21 mb-1">
+              <p className="text-white font-normal text-xs">{discount}% Dcto.</p>
+            </div>
+            : null
+        }
+
+        {
+          discounted_price
+            ? <p className="font-headings font-bold text-xl">${discounted_price}MXN <span className="line-through text-surface-500 font-normal text-sm">${price}</span></p>
+            : <p className="font-headings font-bold text-xl">${price}MXN <span className="text-surface-500 font-normal text-sm">{periodicity}</span></p>
+        }
+        {
+          total_payment
+            ? <p className="font-normal text-xs text-surface-500">Pago total: ${total_payment}</p>
+            : null
+        }
+      </div>
+      {
+        checkout_url
+          ? <div className="flex items-end">
+            <Button
+              dark
+              data={{
+                type: "primary",
+                title: "Inscribirme ahora",
+                isExpand: true,
+                disabled: !checkout_url
+              }}
+              onClick={() => {
+                if (checkout_url) {
+                  router.push(checkout_url);
+                } else {
+                  return
+                }
+              }}
+            />
+          </div>
+          : <div className="flex items-center">
+            <p className="text-6 font-bold">Próximamente</p>
+          </div>
+      }
+    </div>
+  )
+
+};
+export default PaymentCard;
