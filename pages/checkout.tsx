4ee5baa505b2e159ce3dcd0dcce70901d894420f
DavidVazquez <vsjd.99@gmail.com>
2024-06-10 16:48:49 -0600

    pass logic to the checkout

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -5,12 +5,20 @@ import ContentFullLayout from "@/layouts/ContentFull.layout"
 import NextPageWithLayout from "@/types/Layout.types"
 import { useEffect, useMemo, useState } from "react"
 import { InscriptionForm } from "@/forms/container/InscriptionForm"
+import Link from "next/link"
 
 const CheckoutPage: NextPageWithLayout = () => {
 
   const flywireAPI = process.env.NEXT_PUBLIC_FLYWIRE_API
   const flywireAPIKEY = process.env.NEXT_PUBLIC_FLYWIRE_API_KEY
   const [flywireLink, setFlywireLink] = useState()
+  const [isValid, setIsValid] = useState(false);
+  const [submit, setSubmit] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
   useEffect(() => {
     const postData = async () => {
       if (flywireAPI && flywireAPIKEY) {
@@ -104,6 +112,13 @@ const CheckoutPage: NextPageWithLayout = () => {
     console.log('flywireLink: ', flywireLink)
   }, [flywireLink])
 
+  const setStatus = ({ loading, error, valid, success }: { loading: boolean, error: string, valid: boolean, success: boolean }) => {
+    setIsVisible(!loading && !error)
+    setIsLoading(loading)
+    setError(error)
+    setIsValid(valid)
+    setIsSuccess(success)
+  }
 
   return <>
     <Head>
@@ -111,7 +126,73 @@ const CheckoutPage: NextPageWithLayout = () => {
     </Head>
     <HeaderFooterLayout breadcrumbs={false}>
       <ContentFullLayout>
-      <InscriptionForm />
+        <div className="grid grid-cols-2 p-6">
+        <InscriptionForm submit={submit} setStatus={setStatus} />
+        <div className="mobile:col-span-2">
+          <div className="border border-surface-300 rounded-lg p-4">
+            <h3 className="font-headings font-bold text-5.5 leading-6">Diplomado en Análisis de Datos</h3>
+            <p className="text-white bg-primary-500 w-23 px-2 py-1 rounded-full text-center my-3">En línea</p>
+            <hr className="text-surface-300" />
+            <div className="flex justify-between mt-2">
+              <p className="font-texts">Opción de pago:</p>
+              <p className="text-surface-500 font-texts">3 parcialidades</p>
+            </div>
+            <div className="flex justify-between my-1">
+              <p className="font-texts">Parcialidades:</p>
+              <p className="text-surface-500 font-texts">$1,523.00 MXN</p>
+            </div>
+            <div className="flex justify-between mb-2">
+              <p className="font-texts">Costo total:</p>
+              <p className="text-surface-500 font-texts">$4,569.00 MXN</p>
+            </div>
+            <hr className="text-surface-300" />
+            <div className="flex justify-between mt-2">
+              <p className="font-texts font-bold text-base leading-6">Parcialidad a pagar:</p>
+              <p className="text-base font-bold">$1,523.00 MXN</p>
+            </div>
+          </div>
+          {/* {
+            residence &&
+            <div className="flex flex-col my-6">
+            <Button
+              dark
+              data={{
+                type: "primary",
+                title: "Inscribirme ahora",
+                isExpand: true,
+                disabled: false
+              }}
+              onClick={() => {
+                onSubmit()
+              }}
+            />
+          </div>
+          } */}
+          {/* {
+            noCurp &&
+            <div className="flex flex-col my-6">
+            <Button
+              dark
+              data={{
+                type: "primary",
+                title: "Inscribirme ahora",
+                isExpand: true,
+                disabled: !isValid
+              }}
+              onClick={() => {
+                onSubmit()
+              }}
+            />
+          </div>
+          } */}
+          <div className="flex mt-2">
+            <p className="text-3.5 leading-5 text-surface-800 font-texts font-normal mr-1">Al llenar tus datos aceptas nuestro</p>
+            <Link href="terminos-y-condiciones" passHref target={"_blank"}>
+              <p className="text-3.5 font-texts font-normal text-sm text-surface-800 underline">Aviso de Privacidad</p>
+            </Link>
+          </div>
+        </div>
+        </div>
         <ContentInsideLayout>
           <div className="flex w-full">
           <div className="w-1/2 h-full mx-auto text-center align-middle">checkout</div>          
