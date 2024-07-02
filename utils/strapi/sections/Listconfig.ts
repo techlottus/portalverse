b8f2f3f89ce4847a66d6da7dbabd47d9b2fbcb6c
David Vazquez Sanchez <vsjd.99@gmail.com>
2024-07-02 16:26:57 -0600

    add vacancies rules for the listconfig

--- utils/strapi/sections/Listconfig.ts
+++ utils/strapi/sections/Listconfig.ts
@@ -1,8 +1,13 @@
 import getBlogEntryPageData from "@/utils/getBlogEntryPageData";
 import getBlogPosts from "@/utils/getBlogPosts";
 import getPodcastEpisodes from "@/utils/getPodcastEpisodes";
+import getVacancies, { Vacancies } from "@/utils/getVacancies";
 import type { BlogPost } from "@/utils/getBlogPosts";
 import type { PodcastEpisode } from "@/utils/getPodcastEpisodes";
+
+type VacanciesData = {
+  vacancies: Array<Vacancies>;
+};
 
 type BlogEntriesData = {
   blogPageSlug: string;
@@ -17,13 +22,17 @@ type ListconfigBase = {
 
 type ListconfigType =
   | {
-      relatesto: "blogentries";
-      data?: BlogEntriesData;
-    }
+    relatesto: "blogentries";
+    data?: BlogEntriesData;
+  }
   | {
-      relatesto: "podcasts";
-      data?: Array<PodcastEpisode>;
-    };
+    relatesto: "podcasts";
+    data?: Array<PodcastEpisode>;
+  }
+  | {
+    relatesto: "vacancies";
+    data?: VacanciesData;
+  };
 
 export type ListconfigData = ListconfigBase & ListconfigType;
 
@@ -58,7 +67,7 @@ export const formatListconfigSection = async (
       const blogPageSlug = blogEntryPage?.data?.attributes?.slug;
       const blogPosts = blogPostsData?.blogPosts?.data;
 
-      if(blogPageSlug && blogPosts) {
+      if (blogPageSlug && blogPosts) {
         section.data = {
           blogPageSlug: blogEntryPage?.data?.attributes?.slug,
           blogPosts,
@@ -76,6 +85,25 @@ export const formatListconfigSection = async (
             : "publicationDate:asc",
       });
       section.data = podcastEpisodes?.podcasts?.data;
+    }
+    case "vacancies": {
+      const vacanciesData = await getVacancies({
+        limit: section?.maxentries,
+        sort:
+          section?.sortdate === "latest"
+            ? "publication_date:desc"
+            : "publication_date:asc",
+      });
+
+      const vacancies = vacanciesData?.vacancies?.data;
+
+      if (vacancies) {
+        section.data = {
+          vacancies
+        };
+      }
+
+      break;
     }
     default:
       return section;
