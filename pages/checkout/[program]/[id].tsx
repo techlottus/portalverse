d208b499b4f23f0439f52af74fd7d9c7a2ec55ff
David Vazquez Sanchez <vsjd.99@gmail.com>
2024-07-05 17:37:55 -0600

    privacy policy fix

--- pages/checkout/[program]/[id].tsx
+++ pages/checkout/[program]/[id].tsx
@@ -317,7 +317,7 @@ const CheckoutPage: NextPageWithLayout<PageProps> = (props: PageProps) => {
               </div>
               <div className="flex mt-3">
                 <p className="text-3.5 leading-5 text-surface-800 font-texts font-normal mr-1">Al llenar tus datos aceptas nuestro</p>
-                <Link href="terminos-y-condiciones" passHref target={"_blank"}> {/* deberia ir a aviso de privacidad???*/}
+                <Link href="/terminos-y-condiciones" passHref target={"_blank"}> {/* deberia ir a aviso de privacidad???*/}
                   <p className="text-3.5 font-texts font-normal text-sm text-surface-800 underline">Aviso de Privacidad</p>
                 </Link>
               </div>
