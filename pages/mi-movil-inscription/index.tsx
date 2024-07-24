f590f446bd97fa3f4a05969aa916fe8203a48e6b
alan llamas <alanllamasg@gmail.com>
2024-07-23 20:16:29 -0500

    feat: added connections to SF

--- pages/mi-movil-inscription/index.tsx
+++ pages/mi-movil-inscription/index.tsx
@@ -7,6 +7,8 @@ import { msalInstance } from "@/utils/authConfig";
 import { useIsAuthenticated } from "@azure/msal-react";
 import { useRouter } from "next/router";
 import { MiMovilInscriptionForm } from "@/forms/container/MiMovilInscriptionForm";
+import axios from "axios";
+import { getTokenForms } from "@/utils/getTokenForms";
 
 
 
@@ -50,6 +52,8 @@ const MiMovilInscription: NextPageWithLayout<any> = (props: any) => {
   const [isValidCurp, setIsValidCurp] = useState(false);
   const [curpError, setCurpError] = useState(false);
   const [ program, setProgram ] = useState<any>(null);
+  const [tokenActive, setTokenActive] = useState<string>("");
+
 
   
   const setStatus = ({ loading, valid, success }: { loading: boolean, valid: boolean, success: boolean }) => {
@@ -66,16 +70,28 @@ const MiMovilInscription: NextPageWithLayout<any> = (props: any) => {
     phone: "",
     birthdate: "",
     gender: "",
-    residence: "",
-    adviser: ""
+    residence: ""
   }
 
   const [personalData, setPersonalData] = useState(initialData);
 
+  const {
+    isLoading: isLoadingToken,
+    isError: isErrorToken,
+    token,
+  } = getTokenForms();
 
 
   const isAuthenticated = useIsAuthenticated();
   const router = useRouter()
+
+  useEffect(() => {
+    console.log(token);
+    
+    if (!isLoadingToken && !isErrorToken && !!Object.keys(token).length) {
+      setTokenActive(`${token.token_type} ${token.access_token}`);
+    }
+  }, [isLoadingToken, isErrorToken, token]);
 
   useEffect(() => {
     if (!isAuthenticated) {
@@ -84,15 +100,88 @@ const MiMovilInscription: NextPageWithLayout<any> = (props: any) => {
         // router.push('/mi-movil-inscription/login')
     }
   }, [isAuthenticated]);
+  const getLeadModality = (modality: string) => {
+  switch (modality) {
+    case "Presencial": return "Presencial";
+    case "Online": return "Online";
+    case "Flex": return "Online"; // Applies to "UANE" and "UTEG" offer.
+    case "Semipresencial": return "Semipresencial"; // Applies to "ULA" offer.
+    default: return "";
+  }
+};
+
 
   const onSubmit = () => {
     const data = {
       ...personalData,
+      curp,
       metadata: program
     }
     console.log('data final:',data);
+    sendInscriptionData(data)
     
   }
+
+  const sendInscriptionData = async (data: any) => {
+    const endpoint = process.env.NEXT_PUBLIC_MI_MOVIL_INSCRIPTION;
+    //  https://lottus--develop.sandbox.my.salesforce.com/services/apexrest/alumno_convenio_empresarial_inscripcion
+
+    // const nombre = personalData?.name;
+    // const apellidoPaterno = personalData?.last_name;
+    // const telefono = personalData?.phone;
+    // const email = personalData?.email;
+    // const lineaNegocio = program?.lineaNegocio || process.env.NEXT_PUBLIC_BUSINESS_UNIT;
+    // const modalidad = getLeadModality(program?.modality);
+    // const nivel = program?.level;
+    // const campus = program?.campus;
+    // const programa = program?.program;
+    // const params = `nombre=${nombre}&apellidoPaterno=${apellidoPaterno}&telefono=${telefono}&email=${email}&lineaNegocio=${lineaNegocio}&modalidad=${modalidad}&nivel=${nivel}&campus=${campus}&programa=${programa}`;
+
+    setIsLoading(true);
+    const body = {
+      "nombre": data.name,
+      "apellidoPaterno": data.last_name,
+      "apellidoMaterno": data.second_last_name,
+      "genero": data.gender,
+      "fechaNacimiento": data.birthdate,
+      "estadoCivil": "Soltero",
+      "telefono": data.phone,
+      "celular": data.phone,
+      "email": data.email,
+      "modalidad": data.metadata.SFmodality,
+      "nivel": data.metadata.SFlevel,
+      "campus": data.metadata.SFcampus,
+      "programa": data.metadata.SFprogram,
+      "nacionalidad": data.residence,
+      "curp": data.curp,
+      "lineaNegocio": data.metadata.SFline,
+      "claveCargoBanner": data.metadata.BNRcharge,
+      "empresaConvenio": "MI MÓVIL"
+    }
+    console.log('body: ', body);
+    
+
+    await axios.post(`${endpoint}`, body,{
+      headers: {
+        Authorization: tokenActive,
+        'Content-Type': 'application/json;charset=UTF-8'
+      }
+    })
+      .then((res: any) => {
+        console.log('res?.data: ', res?.data);
+        if(res?.data?.Exitoso !== "TRUE") {
+          throw new Error();
+        }
+        // router.push(`/mi-movil-inscription`);
+        
+        window.location.reload()
+      })
+      .catch((err: any) => {
+        setIsLoading(false);
+        setError(err.message);
+      })
+  }
+
   
 
   return (
@@ -107,7 +196,6 @@ const MiMovilInscription: NextPageWithLayout<any> = (props: any) => {
         </section>
         <section className="w-full flex justify-center mt-20">
           <section>
-
             <MiMovilInscriptionForm
               submit={submit}
               setStatus={setStatus}
