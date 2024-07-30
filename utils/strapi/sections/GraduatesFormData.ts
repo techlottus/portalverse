382e252e3a201da2134d8051487e04e70f104b9a
David Vazquez Sanchez <vsjd.99@gmail.com>
2024-07-30 15:14:19 -0600

    graduates form section data type

---
+++ utils/strapi/sections/GraduatesFormData.ts
@@ -0,0 +1,12 @@
+export type GraduatesFormData = {
+  type: "ComponentSectionsGraduatesForm";
+  title: string;
+  subtitle: string;
+};
+
+export const GRADUATES_FORM = `
+... on ComponentSectionsGraduatesForm {
+  title
+  subtitle            
+}
+`;
