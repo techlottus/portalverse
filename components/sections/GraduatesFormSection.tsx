5ed08333a76c9d783ebe6054d28123bb0c9242f1
David Vazquez Sanchez <vsjd.99@gmail.com>
2024-07-30 17:06:19 -0600

    go top banner flag to space the form

--- components/sections/GraduatesFormSection.tsx
+++ components/sections/GraduatesFormSection.tsx
@@ -3,20 +3,22 @@ import Container from "../../layouts/Container.layout";
 import RichtText from '@/old-components/Richtext/Richtext';
 import { GraduatesFormData } from "../../utils/strapi/sections/GraduatesFormData";
 import GraduatesForm from "@/forms/container/GraduatesForm"
+import cn from "classnames"
 
 const GraduatesFormSection: React.FC<GraduatesFormData> = (props: GraduatesFormData) => {
   const {
     title,
     subtitle,
+    goTopBanner
   } = props;
 
   return <>
     <Container classNames="flex mobile:flex-col tablet:flex-col desktop:gap-6">
-      <div className="desktop:w-1/2 mobile:w-full my-auto">
+      <div className={cn("desktop:w-1/2 my-auto mobile:w-full", { "desktop:mt-[-3.5em]": goTopBanner })}>
         <h3 className="font-headings text-5xl mb-2 font-bold">{title}</h3>
         <RichtText font="light" data={{ content: subtitle }} classNames="text-xl" />
       </div>
-      <div className="desktop:w-1/2 mobile:w-full">
+      <div className={cn("desktop:w-1/2 mobile:w-full", { "desktop:mt-[-13em] desktop:pr-6": goTopBanner })}>
         <GraduatesForm />
       </div>
     </Container>
--- utils/strapi/sections/GraduatesFormData.ts
+++ utils/strapi/sections/GraduatesFormData.ts
@@ -2,11 +2,13 @@ export type GraduatesFormData = {
   type: "ComponentSectionsGraduatesForm";
   title: string;
   subtitle: string;
+  goTopBanner: boolean;
 };
 
 export const GRADUATES_FORM = `
 ... on ComponentSectionsGraduatesForm {
   title
-  subtitle            
+  subtitle
+  goTopBanner
 }
 `;
