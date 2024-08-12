47fba9fd16884880f14044699541ee089a0d5448
Maria-cris <maria.delacruz@lottuseducation.com>
2024-08-12 14:28:12 -0600

    core: added rederer faqImage

---
+++ components/FaqImage.tsx
@@ -0,0 +1,48 @@
+import { FC } from "react";
+import Container from "@/layouts/Container.layout";
+import cn from "classnames";
+import Button from "@/old-components/Button/Button";
+import { useRouter } from "next/router";
+import { FaqImageSection } from "@/utils/strapi/sections/FaqImage";
+import FAQ from "./sections/FAQ";
+import ContentLayout from "@/layouts/Content.layout";
+
+const FaqImage: FC<FaqImageSection> = (props: FaqImageSection) => {
+  const {
+    title,
+    image,
+    imgPosition,
+    faq,
+    button
+  } = props;
+
+  const router = useRouter();
+
+
+
+  return (
+    <section id="faq-image-section" className="desktop:px-6  mobile:mx-0">
+      <h2 id="faq-image-title" className="font-headings font-bold leading-tight text-10 tablet:text-6 mobile:text-6 mobile:px-6 tablet:px-6 mb-6">
+        {title}
+      </h2>
+      <div id="faq-image-content" className="flex-col">
+        <div className={cn("flex mobile:flex-col tablet:flex-col mb-12 tablet:mb-6 mobile:mb-6",{["flex-row-reverse"]:imgPosition=="right"})}>
+          <div className="desktop:w-1/2 tablet:hidden mobile:hidden ">
+            <img className="w-full" src={image?.data?.attributes?.url} />
+          </div>
+          <div className="desktop:w-1/2">
+            {faq && <FAQ {...faq} />}
+          </div>
+        </div>
+        <div className="flex justify-center">
+          {button && <Button data={button} onClick={() => {
+              router.push(button.CTA)
+            }}/>}
+        </div>
+      </div>
+
+    </section>
+  )
+}
+
+export default FaqImage
---
+++ utils/strapi/sections/FaqImage.ts
@@ -0,0 +1,75 @@
+import { ButtonConfig } from "@/types/Button.types";
+import { StrapiImage } from "@/types/strapi/common";
+
+type FaqItem = {
+  attributes: {
+    question: string;
+    answer: string;
+  };
+};
+
+export type FAQSection = {
+  type: "ComponentSectionsFaqSection"
+  title?: string;
+  ctaText: string;
+  ctaUrl: string;
+  component: "accordion" | "card" | "list";
+  faqCategory: {
+    data: {
+      attributes: {
+        faqs: {
+          data: Array<FaqItem>;
+        };
+      };
+    };
+  };
+};
+export type FaqImageSection = {
+  type: "ComponentSectionsFaqImage";
+  title?: string;
+  image?: StrapiImage;
+  imgPosition?: "left" | "right";
+  faq?: FAQSection;
+  button?: ButtonConfig & {CTA:string};
+}
+
+export const FAQ_IMAGE = `
+...on ComponentSectionsFaqImage {
+  title
+  image{
+    data{
+      attributes{
+        url
+      }
+    }
+  }
+  imgPosition
+  faq{
+    faqCategory {
+      data {
+        attributes {
+          title
+          faqs(pagination:{start: 0, limit: -1}) {
+            data {
+              attributes {
+                question
+                answer
+              }
+            }
+          }
+        }
+      }
+    }
+    ctaText
+    ctaUrl
+    component
+  }
+  button{
+    size
+    title:label
+    CTA
+    iconName
+    type:variant
+  }
+}
+`;
