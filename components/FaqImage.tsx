51822b1e26ad22544691bd07cca25d6c021a93ce
Maria-cris <maria.delacruz@lottuseducation.com>
2024-08-13 15:06:37 -0600

    core: space y

--- components/FaqImage.tsx
+++ components/FaqImage.tsx
@@ -25,7 +25,7 @@ const FaqImage: FC<FaqImageSection> = (props: FaqImageSection) => {
       <h2 id="faq-image-title" className="font-headings font-bold leading-tight text-10 tablet:text-6 mobile:text-6 mobile:px-6 tablet:px-6 mb-6">
         {title}
       </h2>
-      <div id="faq-image-content" className="flex-col">
+      <div id="faq-image-content" className="flex-col space-y-10">
         <div className={cn("flex mobile:flex-col tablet:flex-col ",{["flex-row-reverse "]:imgPosition=="right"})}>
           <div className={ cn("tablet:hidden mobile:hidden desktop:w-1/2")}>
             <img className="w-full" src={image?.data?.attributes?.url} />
