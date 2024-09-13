d6ff23d4e2c50e6826c9a5fff2b9bc2fa2e3e9ab
Maria-cris <maria.delacruz@lottuseducation.com>
2024-09-13 15:11:09 -0600

    core: added alertinfo and banners

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -7,6 +7,9 @@ import classNames from 'classnames';
 import { CaretDownIcon } from '@radix-ui/react-icons';
 import Link from "next/link"
 import { useRouter } from "next/router";
+import Banner from "./Banner";
+import RepeatableBannerSection from "./RepeatableBannerSection";
+import AlertInfo from "./AlertInfo";
 
 const Header = (props: MenuType) => {
   const {
@@ -84,119 +87,119 @@ const Header = (props: MenuType) => {
                 "label": "Negocios y Empresariales",
                 "bold": true,
                 "href": null,
-                "subitems": []
+                "items": []
               },
               {
                 "id": "2",
                 "label": "Licenciatura en Administración de Empresas",
                 "bold": false,
                 "href": null,
-                "subitems": []
+                "items": []
               },
               {
                 "id": "1",
                 "label": "Licenciatura Ejecutiva en Comercio Internacional",
                 "bold": false,
                 "href": null,
-                "subitems": []
+                "items": []
               },
               {
                 "id": "4",
                 "label": "Licenciatura en Contaduría y Finanzas",
                 "bold": false,
                 "href": null,
-                "subitems": []
+                "items": []
               },
               {
                 "id": "6",
                 "label": "Licenciatura en Mercadotecnia",
                 "bold": false,
                 "href": null,
-                "subitems": []
+                "items": []
               },
               {
                 "id": "5",
                 "label": "Licenciatura en Turismo",
                 "bold": false,
                 "href": null,
-                "subitems": []
+                "items": []
               },
               {
                 "id": "7",
                 "label": "Salud",
                 "bold": true,
                 "href": null,
-                "subitems": []
+                "items": []
               },
               {
                 "id": "8",
                 "label": "Licenciatura en Psicología",
                 "bold": false,
                 "href": null,
-                "subitems": []
+                "items": []
               },
               {
                 "id": "9",
                 "label": "Licenciatura en Nutrición",
                 "bold": false,
                 "href": null,
-                "subitems": []
+                "items": []
               },
               {
                 "id": "11",
                 "label": "Licenciatura en Fisioterapia",
                 "bold": false,
                 "href": null,
-                "subitems": []
+                "items": []
               },
               {
                 "id": "10",
                 "label": "Licenciatura en Cirujano Dentista",
                 "bold": false,
                 "href": null,
-                "subitems": []
+                "items": []
               },
               {
                 "id": "12",
                 "label": "Ingeniería y Tecnologías",
                 "bold": true,
                 "href": null,
-                "subitems": []
+                "items": []
               },
               {
                 "id": "13",
                 "label": "Licenciatura en Ingeniería en Sistemas Computacionales",
                 "bold": false,
                 "href": null,
-                "subitems": []
+                "items": []
               },
               {
                 "id": "14",
                 "label": "Licenciatura Ejecutiva en Comercio Internacional",
                 "bold": false,
                 "href": null,
-                "subitems": []
+                "items": []
               },
               {
                 "id": "17",
                 "label": "Licenciatura en Contaduría y Finanzas",
                 "bold": false,
                 "href": null,
-                "subitems": []
+                "items": []
               },
               {
                 "id": "16",
                 "label": "Licenciatura en Mercadotecnia",
                 "bold": false,
                 "href": null,
-                "subitems": []
+                "items": []
               },
               {
                 "id": "15",
                 "label": "Licenciatura en Turismo",
                 "bold": false,
                 "href": null,
-                "subitems": []
+                "items": []
               }
             ]
           }
@@ -210,7 +213,7 @@ const Header = (props: MenuType) => {
         {
           "title": "Banner 1",
           "subtitle": "contenido de banner 1",
-          "desktopRatio": "7/2",
+          "desktopRatio": "2/1",
           "desktopImage": {
             "data": {
               "attributes": {
@@ -228,7 +231,7 @@ const Header = (props: MenuType) => {
         {
           "title": "Banner 2",
           "subtitle": "contenido de banner 2",
-          "desktopRatio": "7/2",
+          "desktopRatio": "2/1",
           "desktopImage": {
             "data": {
               "attributes": {
@@ -270,7 +273,7 @@ const Header = (props: MenuType) => {
     return (
       <NavigationMenu.List className="flex w-full items-center justify-end last:border-none ">
         {/*TODO map function to list items */}
-        {data?.links_button.map((link: any,i:number) =>
+        {data?.links_button.map((link: any, i: number) =>
           <NavigationMenu.Item key={i} className=" border-r border-surface-300 px-3 ">
             <Link href={link.href ? link.href : ""} passHref >
               <p className="font-headings font-normal text-xs text-surface-400 hover:underline cursor-pointer">
@@ -283,35 +286,41 @@ const Header = (props: MenuType) => {
     )
   }
 
-  const SubItems = ({subitems}: {subitems:SubitemsType[]}) => {
+  const SubItems = ({ subitems }: { subitems: SubitemsType[] }) => {
     return (
-      <ul className="flex-col">
-        { subitems?.map((item:any,i:number) => 
-          item?.items.length > 0 ? 
-                <button key={i} className="group hover:border hover:border-surface-400 rounded hover:text-primary-500 px-2 py-2" onMouseEnter={() => setItems(true)} >
-                  <Link href={item?.href ? item.href:""} passHref>
-                    <div className="flex items-center  ">
-                      <p className="group-hover:underline font-normal group-hover:text-primary-500 text-surface-400 font-texts">
-                        {item?.label}
-                      </p>
-                      <span className="font-icons-outlined text-lg group-hover:text-primary-500 text-surface-400 font-bold ml-3 ">arrow_forward_ios</span>
-                    </div>
-                  </Link>
-                </button>
-              : item.bold ?
-                <Link key={i} href={item?.href ? item.href:""} passHref><p className="font-heading text-surface-950 font-semibold py-2">{item.label}</p></Link>
-                :
-                <Link key={i} href={item?.href ? item.href:""} passHref><p className="font-texts text-surface-400 pl-2 font-normal py-2">{item.label}</p></Link>
-    
-          )
-        }
+      <div className="flex relative h-full ">
+        <div className="flex w-full h-full pr-4 ">
+          <ul className="flex-col  w-full h-full">
+            {subitems?.map((item: any, i: number) =>
+              item?.items?.length > 0 ?
+                <>
+                  <button key={i} className="group hover:border hover:border-surface-400 rounded hover:text-primary-500 px-2 py-2 w-full" onMouseEnter={() => setItems(true)} >
+                    <Link href={item?.href ? item.href : ""} passHref>
+                      <div className="flex items-center justify-between ">
+                        <p className="group-hover:underline font-normal group-hover:text-primary-500 text-surface-400 font-texts">
+                          {item?.label}
+                        </p>
+                        <span className="font-icons-outlined text-lg group-hover:text-primary-500 text-surface-400 font-bold ml-3 ">people</span>
+                      </div>
+                    </Link>
+                  </button >
+                  {/* <div className={classNames("px-6 absolute top-0 left-50 w-full", { ["hidden"]: !items })} tabIndex={-1} onMouseEnter={() => setItems(true)} onMouseLeave={() => setItems(false)}>
+                    <SubItems subitems={item?.items} />
+                  </div> */}
+                </>
+                : item.bold ?
+                  <Link key={i} href={item?.href ? item.href : ""} passHref><p className="font-heading text-surface-950 font-semibold py-2 w-full">{item.label}</p></Link>
+                  :
+                  <Link key={i} href={item?.href ? item.href : ""} passHref><p className="font-texts text-surface-400 pl-2 font-normal py-2 w-full">{item.label}</p></Link>
+            )}</ul></div>
 
-      </ul>
+      </div >
     )
   }
 
   return (
     <div className="absolute top-0 z-20 flex flex-col w-full ">
+      {/* Primer nivel del menú */}
       <NavigationMenu.Root className="flex py-4 px-21  border-b border-surface-300 w-full justify-between">
         <div className="w-18 h-10 bg-logo bg-cover bg-center "></div>
         <div className="flex items-center">
@@ -327,32 +336,53 @@ const Header = (props: MenuType) => {
           </button>
         </div>
       </NavigationMenu.Root>
+      {/* Segundo Nivel delMenu */}
       <NavigationMenu.Root className="h-11 border-b border-surface-300 shadow">
         <NavigationMenu.List className="flex px-21  w-full items-center ">
-          {data.menu_items.map((menu_item: any,i:number) =>
+          {data.menu_items.map((menu_item: any, i: number) =>
             <NavigationMenu.Item key={i}>
               <NavigationMenu.Trigger className="group flex space-x-4 font-headings font-semibold text-surface-900 items-center text-sm border-surface-300 px-3 data-[state=open]:border-b-4 data-[state=open]:border-primary-300 data-[state=open]:text-primary-300 py-3">
                 {menu_item.label} <CaretDownIcon className="relative transition duration-150 ease-out hover:ease-in group-data-[state=open]:rotate-180 group-data-[state=open]:text-primary-300 ml-1" aria-hidden />
               </NavigationMenu.Trigger>
               {
-                menu_item.subitems ?  <NavigationMenu.Content className="h-full bg-transparent shadow-none ">
-                  <div className="w-full h-[1000px] bg-surface-950/30 absolute -z-20 blur-md my-20 overscroll-none overflow-y-hidden "></div>
-                  <div className="bg-surface-0 overflow-y-hidden px-21 py-6" >
-                    <div className="flex">
-                      <div className="flex pr-4 border-r border-surface-400 w-fit h-fit ">
+                menu_item.subitems ?
+                  <NavigationMenu.Content className="h-full bg-transparent shadow-none">
+                    <div className="w-full h-[1000px] bg-surface-950/30 absolute -z-20 blur-md my-20 overscroll-none overflow-y-hidden "></div>
+                    <div className="bg-surface-0 max-h-[1000px] px-21 py-6 h-full w-full" >
+                      {/* items and subitems */}
+                      <div className="flex w-full">
+                        <div className="w-1/4 border-r border-surface-400">
                         <SubItems subitems={menu_item?.subitems} />
-                      </div>
-                      {items &&
-                        <div className="px-6" tabIndex={-1} onMouseEnter={() => setItems(true)} onMouseLeave={() => setItems(false)}>
-                          {/* <SubItems /> */}
                         </div>
-                      }
+                        <div className="w-3/4 px-3">
+                          <div className="flex-col space-y-3 w-full justify-between">
+                            <RepeatableBannerSection {...data?.banners} />
+                            <div className="border-t border-surface-400 py-2">
+                              <AlertInfo {...data?.alert} />
+                            </div>
+                          </div>
+
+                        </div>
+
+                      </div>
+
+
+                      {/* <div className="flex h-full ">
+                        <SubItems subitems={menu_item?.subitems} />  {
+                          !items ?
+                            <div className="flex-col space-y-3 w-3/4 justify-between">
+                               <RepeatableBannerSection {...data?.banners} />
+                               <div className="border-t border-surface-400 py-2">
+                               <AlertInfo {...data?.alert}/> 
+                               </div>
+                            </div>
+                            : null
+                        }                      
+                      </div> */}
+
                     </div>
-                  </div>
-
-                </NavigationMenu.Content>:null
+                  </NavigationMenu.Content> : null
               }
-
             </NavigationMenu.Item>
           )}
 
