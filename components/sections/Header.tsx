e4a53f158c95deeebdcc27b636d84019e85761d9
Maria-cris <maria.delacruz@lottuseducation.com>
2024-10-04 15:10:59 -0600

    core: added mobile styles

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -21,18 +21,21 @@ const Header = (props: MenuType) => {
 
   // Componente de enlaces
   const Links = ({ links }: { links?: (MenuType['links']) }) => (
-    <NavigationMenu.List className="flex w-full items-center justify-end last:border-none">
+    <div className="w-full">
+      <NavigationMenu.List className="flex mobile:space-x-3 w-full desktop:items-center desktop:justify-end desktop:last:border-none">
       {links?.map((link?: any, i?: number) => (
-        <NavigationMenu.Item key={i} className="px-3 border-r border-surface-300">
+        <NavigationMenu.Item key={i} className="mobile:w-1/2 desktop:px-3 mobile:p-4 desktop:border-r mobile:border-2 mobile:rounded desktop:border-surface-300">
           <Link href={link?.href ?? ""} passHref>
-            <p className="font-headings font-normal text-xs text-surface-400 cursor-pointer hover:underline">
+            <p className="font-headings font-normal text-xs desktop:text-surface-400 cursor-pointer hover:underline w-full">
               {link?.text}
             </p>
-            <span className="desktop:hidden mobile:material-symbols-outlined mobile:text-2xl mobile:font-bold mobile:text-end mobile:w-full">arrow_forward</span>
+            <span className="desktop:hidden material-symbols-outlined mobile:text-2xl mobile:font-bold mobile:text-end mobile:w-full">arrow_forward</span>
           </Link>
         </NavigationMenu.Item>
       ))}
     </NavigationMenu.List>
+    </div>
+    
   );
 
   // Componente de subitems
@@ -136,7 +139,7 @@ const Header = (props: MenuType) => {
   return (
     <div className="absolute top-0 z-20 flex flex-col w-full ">
       {/* Primer nivel del menú */}
-      <NavigationMenu.Root className="flex mobile:h-fit desktop:py-4 tablet:py-1 mobile:py-3 desktop:px-21 tablet:px-21 mobile:px-3 desktop:border-b desktop:border-surface-300 w-full desktop:justify-between tablet:justify-between mobile:justify-center mobile:align-middle">
+      <NavigationMenu.Root className="relative flex mobile:h-fit desktop:py-4 tablet:py-1 mobile:py-3 desktop:px-21 tablet:px-21 mobile:px-3 desktop:border-b desktop:border-surface-300 w-full desktop:justify-between tablet:justify-between mobile:justify-center mobile:align-middle">
         <button className="absolute top-0 left-0 px-3 py-3 hidden mobile:flex items-center h-full" onClick={()=>handleHamburguer()}>
         {open && <Icon name="sort" className="w-6 h-6" />}
         {!open && <span className="flex align-middle items-center"><Icon name="close" className="w-3 h-3 mr-1" /> <p className="font-texts font-normal text-sm">Cerrar</p></span>}
@@ -153,15 +156,16 @@ const Header = (props: MenuType) => {
       </NavigationMenu.Root>
 
       {/* Segundo nivel del menú */}
-      <NavigationMenu.Root className="h-9.5 border-b border-surface-300 shadow mobile:hidden">
-        <NavigationMenu.List className="px-21 w-full flex items-center">
+      <NavigationMenu.Root className={classNames("desktop:h-9.5 mobile:h-screen tablet:h-auto desktop:border-b desktop:border-surface-300 desktop:shadow mobile:bg-surface-50 mobile:overscroll-none", { ["mobile:hidden"]: open })}>
+        <NavigationMenu.List className="desktop:px-21 desktop:w-full flex mobile:flex-col desktop:items-center mobile:py-4">
           {menu_items?.map((menu_item, i) => (
             <NavigationMenu.Item key={i}onMouseEnter={()=>{
               setItems(false)
               setSubItems(false)
-              }}>
-              <NavigationMenu.Trigger className="group flex h-9.5 space-x-4 font-headings font-normal text-surface-900 items-center text-sm border-surface-300 px-3 data-[state=open]:border-b-4 data-[state=open]:border-primary-300 data-[state=open]:text-primary-300 py-3">
-                {menu_item.label} <CaretDownIcon className="relative transition duration-150 ease-out hover:ease-in group-data-[state=open]:rotate-180 group-data-[state=open]:text-primary-300 ml-1" aria-hidden />
+              }}
+              className=" mobile:px-6">
+              <NavigationMenu.Trigger className="group flex mobile:border-b mobile:w-full items-center h-9.5 desktop:space-x-4 font-headings font-normal text-surface-900 text-sm border-surface-300 desktop:data-[state=open]:border-b-4 desktop:data-[state=open]:border-primary-300 desktop:data-[state=open]:text-primary-300 py-3 mobile:py-4 desktop:px-3 ">
+                {menu_item.label} <CaretDownIcon className="relative mobile:hidden transition duration-150 ease-out hover:ease-in group-data-[state=open]:rotate-180 desktop:group-data-[state=open]:text-primary-300 ml-1" aria-hidden />
               </NavigationMenu.Trigger>
               {menu_item.items && (
                 <NavigationMenu.Content className="h-full bg-transparent shadow-none w-full">
@@ -186,6 +190,16 @@ const Header = (props: MenuType) => {
               )}
             </NavigationMenu.Item>
           ))}
+          <div className="desktop:hidden p-6 flex flex-col w-full">
+            <button
+              onClick={() => { button?.CTA ? router.push(button?.CTA) : null }}
+              className="px-4 py-3 rounded bg-surface-950 border border-surface-950 text-sm text-surface-100 font-texts hover:bg-surface-50 hover:text-surface-950 w-full">
+              {button?.label}
+            </button>
+            <div className="desktop:hidden py-3 flex mx-auto w-full">
+              <Links links={links} />
+            </div>
+          </div>
         </NavigationMenu.List>
         <div className="ViewportPosition">
           <NavigationMenu.Viewport className="NavigationMenuViewport" />
