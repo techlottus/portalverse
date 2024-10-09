e4c79a43fb68694faec6bb177be1b14d5faa2dd9
Maria-cris <maria.delacruz@lottuseducation.com>
2024-10-09 11:32:24 -0600

    core: animation tablet and mobile

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -186,8 +186,8 @@ const Header = (props: MenuType) => {
        ALINEACIÓN ITEMS EN DESK 
       
       */}
-      <div className={classNames(" desktop:flex w-full h-full bg-surface-0 mobile:px-0 tablet:px-0 desktop:border-b desktop:border-surface-300 desktop:shadow tablet:fixed mobile:fixed tablet:top-12 mobile:top-12 tablet:overscroll-none mobile:overscroll-none tablet:bg-surface-950/30 tablet:transition-transform mobile:transition-transform", { ["tablet:-translate-x-full mobile:-translate-x-full"]: !open })}>
-        <NavigationMenu.Root value={openContent} onValueChange={setOpenContent} className={classNames("desktop:h-9.5 h-screen overscroll-none desktop:block tablet:data-[state=closed]:hidden tablet:transition-transform mobile:transition-transform",{ ["tablet:-translate-x-full mobile:-translate-x-full"]: !open })}>
+      <div className={classNames(" desktop:flex w-full h-full bg-surface-0 mobile:px-0 tablet:px-0 desktop:border-b desktop:border-surface-300 desktop:shadow tablet:fixed mobile:fixed tablet:top-12 mobile:top-12 tablet:overscroll-none mobile:overscroll-none  ", { ["mobile:-translate-x-full "]: !open , [" tablet:transition-colors tablet:duration-1000 tablet:bg-surface-950/30 tablet:ease-in-out"]:open})}>
+        <NavigationMenu.Root value={openContent} onValueChange={setOpenContent} className={classNames("desktop:h-9.5 h-screen overscroll-none desktop:block tablet:data-[state=closed]:hidden tablet:transition-transform mobile:transition-transform tablet:duration-1000 mobile:duration-1000 mobile:ease-in-out",{ ["tablet:-translate-x-full mobile:-translate-x-full "]: !open })}>
           <NavigationMenu.List className="mobile:w-full h-screen desktop:h-fit desktop:min-w-[1024px] desktop:max-w-[1200px] flex flex-col justify-between desktop:flex-row desktop:items-start py-3 desktop:py-0 desktop:px-21 tablet:max-w-100 bg-surface-0">
             <div className="tablet:overflow-y-auto tablet:overscroll-y-auto mobile:overflow-y-auto mobile:overscroll-y-auto desktop:flex">
               {menu_items?.map((menu_item, i) => (
