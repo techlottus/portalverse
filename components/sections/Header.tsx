738f9513993eb82ec5b2beb4b047a78ed18f0d16
Maria-cris <maria.delacruz@lottuseducation.com>
2024-10-14 11:44:39 -0600

    core: columns subitems without borders

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -71,7 +71,9 @@ const Header = (props: MenuType) => {
         <ul className={classNames("flex flex-col w-full h-full mobile:h-fit tablet:h-fit desktop:pr-6 mobile:space-y-2 tablet:space-y-2", { ["desktop:w-[273px] "]: isSub })} tabIndex={-1}  >
           {list?.map((item: any, i: number) =>
             item?.items?.length > 0 ? (
-              <button key={i} className={classNames("group rounded desktop:px-3 py-2 w-full desktop:border  desktop:border-surface-50 desktop:hover:border-surface-200 text-primary-500 desktop:hover:bg-surface-0", { ["desktop:border-surface-200 desktop:bg-surface-0"]: (!isSub && item.id === itemSelected && items) || (isSub && item.id === itemSubSelected && subItems) })}
+              <button key={i} className={classNames("group py-2 w-full",{
+                [" rounded desktop:px-3 desktop:border desktop:border-surface-50 desktop:hover:border-surface-200 desktop:hover:bg-surface-0"]:!isSub,
+                 ["desktop:border-surface-200 desktop:bg-surface-0"]: (!isSub && item.id === itemSelected && items) || (isSub && item.id === itemSubSelected && subItems) })}
                 onMouseEnter={() => {
                   handleMouseEnter(item, isSub, true, item)
                   isSub ? setSubItemSelected(subItemList.id) : setItemSelected(itemList.id)
@@ -87,19 +89,20 @@ const Header = (props: MenuType) => {
               <Link key={i} href={item.href ?? ""} passHref onMouseEnter={() => {
                 isSub ? setSubItems(false) : setItems(false)
               }} >
-                <p className={classNames("py-2 w-full rounded desktop:border desktop:border-surface-50 desktop:hover:border-surface-200 desktop:hover:bg-surface-0", { 
+                <p className={classNames("py-2 w-full",{
+                  ["desktop:px-3 rounded desktop:border desktop:border-surface-50 desktop:hover:border-surface-200 desktop:hover:bg-surface-0"]:!isSub,
                   ["font-heading text-surface-950 font-semibold"]: item.bold, 
-                  ["font-texts text-surface-500 desktop:hover:text-primary-500 font-normal desktop:px-3"]: !item.bold,
-                   ["desktop:text-sm"]: isSub })}>{item.label}</p>
+                  ["font-texts text-surface-500 desktop:hover:text-primary-500 font-normal "]: !item.bold,
+                  ["desktop:text-sm"]: isSub })}>{item.label}</p>
               </Link>
             )
           )}
-          {linkHref && <Link href={linkHref ?? ""} passHref onMouseEnter={() => {
+          {linkText && <Link href={linkHref ?? ""} passHref onMouseEnter={() => {
             isSub ? setSubItems(false) : setItems(false)
             console.log(linkHref)
           }
           } className="mobile:hidden">
-            <div className="py-2 w-full font-texts text-primary-500 font-normal desktop:px-3 flex align-middle">
+            <div className={classNames("py-2 w-full font-texts text-primary-500 font-normal flex align-middle",{["desktop:px-3"]:!isSub})}>
               <p className={classNames("font-normal hover:underline text-base")}>
                 {linkText ? linkText + " »" : null} </p>
             </div>
