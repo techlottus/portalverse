a3aa1877a1252f1528d32ccd9dbaea34b0d91f1d
Maria-cris <maria.delacruz@lottuseducation.com>
2024-10-16 16:14:32 -0600

    core: items

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -97,11 +97,11 @@ const Header = (props: MenuType) => {
               </div>
             </Link>
           </div>}
-        <ul className={classNames("flex flex-col w-full h-full mobile:h-fit tablet:h-fit desktop:pr-6 mobile:space-y-2 tablet:space-y-2", { ["desktop:w-[273px] desktop:space-y-3 "]: isSub })} tabIndex={-1}  >
+        <ul className={classNames("flex flex-col w-full h-full mobile:h-fit tablet:h-fit desktop:pr-6 mobile:space-y-3 tablet:space-y-3", { ["desktop:w-[273px] desktop:space-y-3 "]: isSub })} tabIndex={-1}  >
           {list?.map((item: any, i: number) =>
             item?.items?.length > 0 ? (
               <button key={i} className={classNames("group w-full", {
-                [" rounded-lg desktop:px-3 desktop:border desktop:border-surface-50 desktop:hover:border-surface-200 desktop:hover:bg-surface-0 py-2"]: !isSub,
+                [" rounded-lg desktop:px-3 desktop:border desktop:border-surface-50 desktop:hover:border-surface-200 desktop:hover:bg-surface-0 desktop:py-2"]: !isSub,
                 ["desktop:border desktop:!border-surface-200 desktop:bg-surface-0 "]: (!isSub && item.id === itemSelected && items)
               })}
                 onMouseEnter={() => {
@@ -121,7 +121,7 @@ const Header = (props: MenuType) => {
               <Link key={i} href={item.href ?? ""} passHref onMouseEnter={() => {
                 isSub ? setSubItems(false) : setItems(false)
               }} >
-                <p className={classNames("py-2 w-full", {
+                <p className={classNames("mobile:py-2 tablet:py-2 w-full", {
                   ["desktop:px-3 rounded-lg desktop:border desktop:border-surface-50 desktop:hover:border-surface-200 desktop:hover:bg-surface-0"]: !isSub,
                   ["font-texts text-surface-950 font-semibold"]: item.bold,
                   ["font-texts text-surface-500 desktop:hover:text-primary-500 font-normal "]: !item.bold,
