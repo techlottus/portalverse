306ada3cf8ef19c55e97c5cf008d1ef945ce18ec
Maria-cris <maria.delacruz@lottuseducation.com>
2024-10-16 15:12:27 -0600

    core: fix height

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -9,11 +9,14 @@ import classNames from "classnames";
 import BannerPortalverseWrapper from "../BannerPortalverseWrapper";
 import Icon from "@/old-components/Icon"
 import { link } from "fs/promises";
+import Aspect from "../Aspect";
+import { AnyARecord } from "dns";
 
 // Componente principal Header
 const Header = (props: MenuType) => {
-  const { name, links, button, menu_items, banners, alert, social_medias={data:[]} } = props;
+  const { name, links, button, menu_items, banners, alert, social_medias = { data: [] } } = props;
   const router = useRouter();
+  console.log("banners: ", banners)
   const [items, setItems] = useState(false);
   const [itemSelected, setItemSelected] = useState(false);
   const [itemSubSelected, setSubItemSelected] = useState(false);
@@ -57,7 +60,7 @@ const Header = (props: MenuType) => {
           <Links links={links} />
         </div>
         <div id='social-media' className="flex w-full justify-center space-x-3 pb-6">
-        {social_medias?.data.length>0 &&
+          {social_medias?.data.length > 0 &&
             social_medias?.data.map((item: any, i: number) => <Link key={`social-${item?.attributes?.name}`} href={item?.attributes?.href} passHref target={"_blank"}>
 
               <Icon name={item?.attributes?.icon_name} className="w-8 h-8 text-balck" />
@@ -99,14 +102,16 @@ const Header = (props: MenuType) => {
             item?.items?.length > 0 ? (
               <button key={i} className={classNames("group py-2 w-full", {
                 [" rounded desktop:px-3 desktop:border desktop:border-surface-50 desktop:hover:border-surface-200 desktop:hover:bg-surface-0"]: !isSub,
-                ["desktop:border-surface-200 desktop:bg-surface-0 "]: (!isSub && item.id === itemSelected && items)
+                ["desktop:border desktop:!border-surface-200 desktop:bg-surface-0 "]: (!isSub && item.id === itemSelected && items)
               })}
                 onMouseEnter={() => {
                   handleMouseEnter(item, isSub, true, item)
                   isSub ? setSubItemSelected(subItemList.id) : setItemSelected(itemList.id)
                 }}>
                 <div className="flex items-center justify-between">
-                  <p className={classNames("font-normal  desktop:group-hover:text-primary-500 text-surface-500 font-texts text-wrap text-left text-base", {["desktop:text-primary-500 desktop:underline desktop:underline-offset-1"]: (!isSub && item.id === itemSelected && items) || (isSub && item.id === itemSubSelected && subItems) })}>
+                  <p className={classNames("font-normal  desktop:group-hover:text-primary-500 text-surface-500 font-texts text-wrap text-left text-base", {
+                    ["desktop:text-primary-500 desktop:underline desktop:underline-offset-1"]: (!isSub && item.id === itemSelected && items) || (isSub && item.id === itemSubSelected && subItems)
+                  })}>
                     {item.label}
                   </p>
                   <span className={classNames("material-symbols-outlined text-2xl desktop:group-hover:text-primary-500 text-surface-400 font-bold ml-3", { ["desktop:text-primary-500"]: (!isSub && item.id === itemSelected && items) || (isSub && item.id === itemSubSelected && subItems) })}>chevron_right</span>
@@ -137,29 +142,13 @@ const Header = (props: MenuType) => {
         </ul>
         {isSub &&
           <ButtonLinks className="mb-50 mt-6" />
-          // <div id='button-links-mobile' className="desktop:hidden flex flex-col w-full tablet:max-w-100 mb-50 mt-6">
-          //   <button
-          //     onClick={() => { button?.CTA ? router.push(button?.CTA) : null }}
-          //     className="px-4 py-3 rounded bg-surface-950 border border-surface-950 text-sm text-surface-100 font-texts hover:bg-surface-50 hover:text-surface-950 w-full">
-          //     {button?.label}
-          //   </button>
-          //   <div className="desktop:hidden py-6 flex mx-auto w-full">
-          //     <Links links={links} />
-          //   </div>
-          //   <div id='social-media' className="flex w-full justify-center space-x-6 pb-6">
-          //     <img src="https://bedu-staging-assets.s3.us-west-2.amazonaws.com/UANE/Facebook_e7dc30f6ef.svg" width={24} height={24} />
-          //     <img src="https://bedu-staging-assets.s3.us-west-2.amazonaws.com/UANE/Instagram_9a5c595b0e.svg" width={24} height={24} />
-          //     <img src="https://bedu-staging-assets.s3.us-west-2.amazonaws.com/UANE/Facebook_e7dc30f6ef.svg" width={24} height={24} />
-          //     <img src="https://bedu-staging-assets.s3.us-west-2.amazonaws.com/UANE/Tik_Tok_54b3009338.svg" width={24} height={24} />
-          //   </div>
-          // </div>
         }
       </div>
     );
   }
 
   const SubItemsCols = ({ subitems, linkText, linkHref = "", isSub = true }: { subitems: any; isSub?: boolean, linkText?: string, linkHref?: string }) => (
-    <div className="flex flex-col w-full overflow-x-auto overscroll-x-auto">
+    <div className="flex flex-col w-full overflow-x-auto overscroll-x-auto desktop:max-h-[724px]">
       <ul className={classNames("flex flex-col flex-wrap max-h-100 max-w-[273px]")} tabIndex={-1} onMouseEnter={() => setItems(true)}>
         {subitems.items?.map((item: any, i: number) => (
           <Link key={i} href={item?.href ?? ""} passHref>
@@ -183,15 +172,91 @@ const Header = (props: MenuType) => {
     </div>
   );
 
+  const Banner = ({ textPosition, desktopRatio, desktopImage, overlay, title, subtitle,ctaUrl,ctaText }: { textPosition: string, desktopRatio: string, desktopImage: any, overlay: string, title: string, subtitle: string, ctaUrl:string,ctaText:string}) => {
+    return (
+      <div className="w-t:hidden w-p:hidden rounded">
+        <Aspect ratio={desktopRatio}>
+          <div className="w-full h-full rounded">
+            <div className={classNames("relative flex w-full h-full shrink-0")} >
+              <img className="w-full h-full w-t:hidden object-cover rounded" src={desktopImage?.data?.attributes.url} alt="image" />
+              {
+                overlay &&
+                <div className={classNames("absolute w-full h-full rounded", {
+                  "bg-surface-0 opacity-50": overlay == "white",
+                  "bg-surface-950 opacity-50": overlay == "black"
+                })}></div>
+              }
+              <div id="Banner-content" className={classNames("absolute w-full h-full flex", {
+                "justify-center text-center": textPosition === "center",
+                "justify-end text-right": textPosition === "right",
+                "items-center": textPosition === "middle",
+                "items-center text-center": textPosition === "middle-left",
+                "justify-start items-center": textPosition === "left-center",
+                "justify-end items-center text-end": textPosition === "middle-right",
+                "justify-center items-center text-center": textPosition === "middle-center",
+                "justify-start items-end": textPosition === "left-bottom",
+                "justify-center items-end text-center": textPosition === "center-bottom",
+                "justify-end items-end text-end": textPosition === "right-bottom",
+                "justify-end items-start text-end": textPosition === "right-top",
+              })}>
+                <div className="p-10 flex flex-col space-y-2 ">
+                  {
+                    title && <h3
+                      className={classNames(
+                        "font-headings font-bold text-wrap w-d:text-5.5 w-t:text-5.5 w-p:text-5.5 w-d:leading-15 w-t:leading-7.5 w-p:leading-7.5",
+                        {
+                          "text-surface-0": overlay === "black",
+                        }
+                      )
+                      }
+                    >
+                      {title}
+                    </h3>}
+                  {
+                    subtitle && <p
+                      className={classNames(
+                        "font-texts font-normal",
+                        {
+                          "text-surface-0": overlay === "black"
+                        }
+                      )
+                      }
+                      dangerouslySetInnerHTML={{ __html: String(subtitle) }}
+                    />
+                  }
+                  {ctaUrl && 
+                  <Link passHref
+                  href={ ctaUrl? ctaUrl : "" }>
+                    <div className={classNames("font-texts text-base flex align-middle items-center",{"text-surface-0": overlay === "black"})}>
+                      <p className="underline">{ctaText}</p> 
+                      <span className="ml-1 material-symbols-outlined">arrow_forward</span>
+                    </div>
+                </Link>
+                  }
+
+                </div>
+
+
+              </div>
+            </div>
+          </div>
+        </Aspect>
+      </div>
+
+    )
+  }
+
   // Layout para el contenido de inicio
   const LayoutHome = ({ banners, alert }: { banners: any, alert: any }) => (
     <div className="flex flex-col space-y-3 w-full justify-between h-full mobile:hidden">
       <div className="grid desktop:grid-cols-2 gap-6 tablet:grid-cols-1 mobile:grid-cols-1">
         {
           banners?.map((item: any, i: number) => <div key={`section-banners-${i}`}>
-            <BannerPortalverseWrapper data={item} onClick={() => {
+            {/* <BannerPortalverseWrapper data={item} onClick={() => {
               router.push(item?.ctaUrl);
-            }} />
+            }} /> */}
+            <Banner {...item} />
+
           </div>)
         }
       </div>
@@ -294,14 +359,14 @@ const Header = (props: MenuType) => {
                       <p className="desktop:font-normal font-semibold font-texts text-base group z-20  flex justify-between desktop:justify-normal mobile:border-b tablet:border-b  w-full  items-center desktop:h-[45px] desktop:space-x-4  border-surface-300  desktop:hover:border-b-4 desktop:hover:border-primary-500 desktop:hover:text-primary-500 desktop:py-3 desktop:hover:pb-2 py-4 desktop:px-3  ">{menu_item?.label}</p>
                     </Link>}
                   {menu_item?.items && menu_item?.items?.length > 0 && (
-                    <NavigationMenu.Content id="content" className={classNames("desktop:absolute desktop:top-0 desktop:left-0 desktop:w-full desktop:data-[motion=from-start]:animate-enterFromLeft desktop:data-[motion=from-end]:animate-enterFromRight desktop:data-[motion=to-start]:animate-exitToLeft desktop:data-[motion=to-end]:animate-exitToRight mobile:z-20 tablet:max-w-100 desktop:min-h-[440px] desktop:max-h-[724px] mobile:bg-surface-0 desktop:rounded-xl tablet:min-h-screen mobile:min-h-screen mobile:h-full mobile:overflow-y-auto mobile:overscroll-y-auto desktop:rounded-b-xl ")}
+                    <NavigationMenu.Content id="content" className={classNames("desktop:absolute desktop:top-0 desktop:left-0 desktop:w-full desktop:data-[motion=from-start]:animate-enterFromLeft desktop:data-[motion=from-end]:animate-enterFromRight desktop:data-[motion=to-start]:animate-exitToLeft desktop:data-[motion=to-end]:animate-exitToRight mobile:z-20 tablet:max-w-100 desktop:min-h-[440px]  mobile:bg-surface-0 desktop:rounded-xl tablet:min-h-screen mobile:min-h-screen mobile:h-full mobile:overflow-y-auto mobile:overscroll-y-auto desktop:rounded-b-xl ")}
                     >
                       <div tabIndex={-1} onClick={() => setOpenContent('closed')} className={classNames("fixed top-0 w-full h-full mobile:hidden bg-surface-950/30 -z-20 tablet:-z-30 overscroll-none overflow-y-hidden")}></div>
                       <div tabIndex={-1} onMouseLeave={() => {
                         setOpenContent('closed')
                         clearStates()
                       }
-                      } className="bg-surface-0  desktop:bg-surface-50 desktop:rounded-b-xl h-full desktop:min-h-[440px] desktop:max-h-[724px] desktop:px-21 px-6 desktop:py-6 py-3 w-full tablet:max-w-100 flex desktop:flex-row flex-col desktop:justify-center tablet:z-20 mobile:overflow-y-auto mobile:overscroll-y-auto">
+                      } className="bg-surface-0  desktop:bg-surface-50 desktop:rounded-b-xl h-full desktop:min-h-[440px]  desktop:px-21 px-6 desktop:py-6 py-3 w-full tablet:max-w-100 flex desktop:flex-row flex-col desktop:justify-center tablet:z-20 mobile:overflow-y-auto mobile:overscroll-y-auto">
                         <div className="flex w-full desktop:max-w-[1200px] desktop:min-h-fit mobile:h-full tablet:h-full mobile:flex-col tablet:flex-col ">
                           <div className="">
                             <div className="desktop:hidden flex flex-col border-b border-surface-300 mobile:mb-3 tablet:mb-3">
@@ -340,22 +405,6 @@ const Header = (props: MenuType) => {
                             {!items && <LayoutHome banners={banners} alert={alert} />}
                           </div>
                           <ButtonLinks className="mb-100 py-6" />
-                          {/* <div id='button-links-mobile' className="desktop:hidden flex flex-col w-full tablet:max-w-100 mb-100 py-6">
-                            <button
-                              onClick={() => { button?.CTA ? router.push(button?.CTA) : null }}
-                              className="px-4 py-3 rounded bg-surface-950 border border-surface-950 text-sm text-surface-100 font-texts hover:bg-surface-50 hover:text-surface-950 w-full">
-                              {button?.label}
-                            </button>
-                            <div className="desktop:hidden py-6 flex mx-auto w-full">
-                              <Links links={links} />
-                            </div>
-                            <div id='social-media' className="flex w-full justify-center space-x-3 pb-6">
-                              <img src="https://bedu-staging-assets.s3.us-west-2.amazonaws.com/UANE/Facebook_e7dc30f6ef.svg" width={24} height={24} />
-                              <img src="https://bedu-staging-assets.s3.us-west-2.amazonaws.com/UANE/Instagram_9a5c595b0e.svg" width={24} height={24} />
-                              <img src="https://bedu-staging-assets.s3.us-west-2.amazonaws.com/UANE/twitter_0578eb0533.svg" width={24} height={24} />
-                              <img src="https://bedu-staging-assets.s3.us-west-2.amazonaws.com/UANE/Tik_Tok_54b3009338.svg" width={24} height={24} />
-                            </div>
-                          </div> */}
                         </div>
                         {items && <div className="desktop:hidden flex flex-col absolute top-0 w-full bg-surface-0 overflow-y-auto overscroll-auto h-full left-full  transition-transform ease-in duration-700 -translate-x-full z-30 ">
                           <SubItems list={itemList?.items} isSub linkText={menu_item?.label} linkHref={menu_item?.href} label={itemList?.label} />
@@ -371,22 +420,7 @@ const Header = (props: MenuType) => {
               ))}
             </div>
             <ButtonLinks className="p-6" />
-            {/* <div id='button-links-mobile' className="desktop:hidden flex flex-col w-full tablet:max-w-100 p-6">
-              <button
-                onClick={() => { button?.CTA ? router.push(button?.CTA) : null }}
-                className="px-4 py-3 rounded bg-surface-950 border border-surface-950 text-sm text-surface-100 font-texts hover:bg-surface-50 hover:text-surface-950 w-full">
-                {button?.label}
-              </button>
-              <div className="desktop:hidden py-6 flex mx-auto w-full">
-                <Links links={links} />
-              </div>
-              <div id='social-media' className="flex w-full justify-center space-x-3 pb-6">
-                <img src="https://bedu-staging-assets.s3.us-west-2.amazonaws.com/UANE/Facebook_e7dc30f6ef.svg" width={24} height={24} />
-                <img src="https://bedu-staging-assets.s3.us-west-2.amazonaws.com/UANE/Instagram_9a5c595b0e.svg" width={24} height={24} />
-                <img src="https://bedu-staging-assets.s3.us-west-2.amazonaws.com/UANE/twitter_0578eb0533.svg" width={24} height={24} />
-                <img src="https://bedu-staging-assets.s3.us-west-2.amazonaws.com/UANE/Tik_Tok_54b3009338.svg" width={24} height={24} />
-              </div>
-            </div> */}
+
 
 
           </NavigationMenu.List>
@@ -394,8 +428,12 @@ const Header = (props: MenuType) => {
           <div className={classNames("absolute w-full desktop:-z-10 desktop:overflow-y-hidden desktop:top-[113px] desktop:left-0 top-0 mobile:left-full mobile:transition-transform mobile:duration-[1000ms] mobile:ease-in-out mobile:-translate-x-full", {
             ["mobile:-translate-x-full"]: openContentMobile == 'open',
             ["mobile:translate-x-full"]: openContentMobile == 'closed',
+            ["hidden"]: openContent=='closed',
           })}>
-            <NavigationMenu.Viewport className={classNames("relative w-full desktop:data-[state=open]:min-h-[440px] desktop:data-[state=open]:max-h-[724px] bg-surface-0 desktop:bg-transparent overflow-hidden tablet:max-w-100 tablet:min-h-full desktop:data-[state=open]:animate-scaleIn desktop:data-[state=closed]:animate-scaleOut")} />
+            <NavigationMenu.Viewport 
+            //@ts-ignore
+            style= {{"--radix-navigation-menu-viewport-height":"",height:"100%"}}
+              className={classNames("relative w-full desktop:data-[state=open]:min-h-[1000px] desktop:data-[state=open]:!h-full desktop:max-h-[1000px] bg-surface-0 desktop:bg-transparent desktop:overflow-visible overflow-hidden tablet:max-w-100 tablet:min-h-full desktop:data-[state=open]:animate-scaleIn desktop:data-[state=closed]:animate-scaleOut")} />
           </div>
         </NavigationMenu.Root>
       </div>
