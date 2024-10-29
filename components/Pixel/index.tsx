dcf599d0402d23b9cd1f5f83c9972b72822224c4
alan llamas <alanllamasg@gmail.com>
2024-10-29 17:15:35 -0600

    chore: removed unused head import

--- components/Pixel/index.tsx
+++ components/Pixel/index.tsx
@@ -1,7 +1,6 @@
 
 import React, { useEffect } from "react";
 import Script from "next/script";
-import Head from "next/head";
 import { useRouter } from "next/router";
 import * as gtag from "@/lib/gtag";
 import * as fbq from "@/lib/fb-pixel";
