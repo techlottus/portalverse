d334c939f9055656f5ab7082e130b99a5708fc45
Maria-cris <maria.delacruz@lottuseducation.com>
2024-11-12 17:54:43 -0600

    styles in button

--- pages/checkout/[program]/[id].tsx
+++ pages/checkout/[program]/[id].tsx
@@ -258,7 +258,7 @@ const CheckoutPage: NextPageWithLayout<PageProps> = (props: PageProps) => {
                                <div className="mt-4">
                                  <button
                                    type="button"
-                                   className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                                   className="inline-flex justify-center rounded-md border border-transparent bg-primary-300 px-4 py-2 text-sm font-medium text-surface-0 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                                    onClick={closeModal}
                                  >
                                    cerrar modal
