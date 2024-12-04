187fbc002e1ed146580e1f4f96d27be7f49d2e7f
Maria-cris <maria.delacruz@lottuseducation.com>
2024-12-04 14:03:34 -0600

    coer

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -407,10 +407,10 @@ const Header = (props: MenuType) => {
                           </div>
                           <ButtonLinks className="mb-100 py-6" />
                         </div>
-                        {items && openContent!=='closed' && open && <div className={classNames("desktop:hidden flex flex-col  top-0 left-0 w-full bg-surface-0 overflow-y-auto overscroll-auto h-full  z-30 mobile:bg-error-500",{["hidden"]:!open, ["absolute"]:open})}>
+                        {items && openContent!=='closed' && open && <div className={classNames("desktop:hidden flex flex-col  top-0 left-0 w-full bg-surface-0 overflow-y-auto overscroll-auto h-full  mobile:bg-error-500",{["hidden"]:!open, ["absolute z-20"]:open})}>
                           <SubItems list={itemList?.items} isSub linkText={itemList?.linkText} linkHref={itemList?.href} label={itemList?.label} />
                         </div>}
-                        {subItems && openContent!=='closed' && open && <div className={classNames("desktop:hidden flex flex-col absolute top-0 left-0 w-full bg-surface-0 overflow-y-auto overscroll-auto h-full  z-30 mobile:bg-primary-500 ",{["hidden"]:!open, ["absolute"]:open})}>
+                        {subItems && openContent!=='closed' && open && <div className={classNames("desktop:hidden flex flex-col absolute top-0 left-0 w-full bg-surface-0 overflow-y-auto overscroll-auto h-full   mobile:bg-primary-500 ",{["hidden"]:!open, ["absolute z-30"]:open})}>
                           <SubItems list={subItemList?.items} isSub linkText={subItemList?.linkText} linkHref={subItemList?.href} label={subItemList?.label} />
                         </div>}
 
