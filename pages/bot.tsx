a60cc8931b4c8c8b1c03db878549975e2ea68750
alan llamas <alanllamasg@gmail.com>
2024-12-27 12:09:45 -0600

    chore: clenaed bot styles

--- pages/bot.tsx
+++ pages/bot.tsx
@@ -3,7 +3,7 @@ const Bot = ( ) => {
 
 
   return <>
-    <iframe src={`https://webchat.botframework.com/embed/bot-lt-prod-eus-openai-02?s=${process.env.NEXT_PUBLIC_BOT_API_KEY}`} className="w-1/3 p-10 mx-40 min-h-170.75"></iframe>
+    <iframe src={`https://webchat.botframework.com/embed/bot-lt-prod-eus-openai-02?s=${process.env.NEXT_PUBLIC_BOT_API_KEY}`} className="desktop:w-1/3 p-10 desktop:mx-40 h-170.75 mobile:w-full"></iframe>
   </>
 };
 
